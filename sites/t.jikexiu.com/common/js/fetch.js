6bec8a89f0b5df12fa73a37c7f634e17148f4ae5
hbbaly <34227573+hbbaly@users.noreply.github.com>
2018-07-03 16:11:43 +0800

    eslint

---
+++ sites/t.jikexiu.com/common/js/fetch.js
@@ -0,0 +1,32 @@
+
+import Axios from './basic'
+/**
+ * @decription      公共数据获取接口
+ * @params {String} url      接口地址
+ * @params {Object} params   request参数
+ * @params {Object} isToken  是否需要token
+*/
+export const fetch = (url, params = {}, loading = false) => {
+  return new Promise((resolve, reject) => {
+    // 接口请求为post方式
+    Axios.post(url, params).then(({ data }) => {
+      if (data.code !== 200) {
+        MIP.setData({alertMsg: {
+          errorTitle: data.msg,
+          isError: true
+        }})
+      }
+
+      resolve(data)
+    }).catch(err => {
+      // 错误抛出
+      MIP.setData({loading: false,
+        alertMsg: {
+          errorTitle: '请求错误',
+          isError: true
+        }})
+      reject(err)
+      // throw new Error(err)
+    })
+  })
+}
