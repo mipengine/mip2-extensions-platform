3fc81c5f700e5ad6b88fdd0591437352e936a1ae
zhuguoxi <zhuguoxi@baidu.com>
2018-07-09 16:51:54 +0800

    first commponent

---
+++ sites/inservice.example.com/components/mip-imagetest/mip-imagetest.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="wrapper">
+    <h1>MIP 2.0 component example</h1>
+    <mip-img
+      layout="responsive"
+      width="350"
+      height="263"
+      src="https://www.mipengine.org/static/img/sample_01.jpg"/>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  margin: 0 auto;
+  text-align: center;
+}
+</style>
+
+<script>
+export default {
+  mounted () {
+    console.log('This is my first custom component !')
+  }
+}
+</script>
