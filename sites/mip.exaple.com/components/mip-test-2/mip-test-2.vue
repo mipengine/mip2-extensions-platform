19f20206be8fa4fc2c32d4e00dda6278e133911e
zhuguoxi <zhuguoxi@baidu.com>
2018-07-11 11:25:38 +0800

    firstcommit

---
+++ sites/mip.exaple.com/components/mip-test-2/mip-test-2.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="wrapper">
+    <h1>MIP 2.0 component example</h1>
+    <h3>{{ldata.name}}</h3>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  margin: 0 auto;
+  text-align: center;
+}
+</style>
+
+<script>
+export default {
+  props: {
+    ldata: {
+      type: Object,
+      required: true
+    }
+  },
+  mounted () {
+    console.log(this.ldata)
+    console.log('This is my first custom component !')
+  }
+}
+</script>
---
+++ sites/mip.exaple.com/components/mip-test/mip-test.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="wrapper">
+    <h1>122121121</h1>
+    <h3>This is my first custom component !</h3>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  margin: 0 auto;
+  text-align: center;
+}
+</style>
+
+<script>
+export default {
+  mounted () {
+    console.log('This is my first custom component !')
+    setTimeout(()=>{
+      MIP.viewer.open('./index2.html', {isMipLink:true})
+    },3000)
+  }
+}
+</script>
