77d86da754b366aea23432eafc33ea7dbe6a7a3f
beanLau <522405475@qq.com>
2018-07-20 15:30:36 +0800

    组建内部逻辑修改

--- sites/wangxiao.cn/components/mip-zdwx-orderlist/mip-zdwx-orderlist.vue
+++ sites/wangxiao.cn/components/mip-zdwx-orderlist/mip-zdwx-orderlist.vue
@@ -163,14 +163,14 @@ export default {
   methods: {
     goEvaluate (order, index) {
       let goodsId = order.goodsId || ''
-      window.MIP.viewer('payorder.jsp?goodsId=' + goodsId)
+      window.MIP.viewer('evaluate.jsp?orderId=' + orderId)
     },
     goStudy (order, index) {
       window.top.location.href = order.url
     },
     goPay (order, index) {
       let orderId = order.orderNumber || ''
-      window.MIP.viewer('evaluate.jsp?orderId=' + orderId)
+      window.MIP.viewer('payorder.jsp?goodsId=' + goodsId)
     },
     cancelOrder (order, index) {
       let orderId = order.orderNumber || ''
