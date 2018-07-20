2ac39f8bbbeade4e2e0ebace6f881dfc88d1b070
Amada <943656412@qq.com>
2018-07-20 09:29:35 +0800

    增加xzw.com

---
+++ sites/xzw.com/components/mip-xzw-remlayout/README.md
@@ -0,0 +1,22 @@
+# mip-xzw-remlayout
+mip-xzw-remlayout 星座屋mip rem布局功能组件
+
+标题|内容
+----|----
+类型|
+支持布局|responsive
+所需脚本|https://c.mipcdn.com/static/v1/mip-xzw-remlayout/mip-xzw-remlayout.js
+
+## 说明
+
+组件功能说明
+
+## 示例
+示例说明
+<mip-xzw-remlayout>
+    自定义内容
+</mip-xzw-remlayout>
+必须在引用该控件的页面添加该标签
+## 属性
+
+属性说明
---
+++ sites/xzw.com/static/public/js/fate_info.js
@@ -0,0 +1,56 @@
+(['<div class="fate_info_box info_box"><div class=\"baseinfo clear\">])
+	
+		<dl class="ft_info fl">
+			<dt class="head">
+				<mip-img src="../static/public/images/prot_icon/xz/a/2.png"></mip-img></dt>
+			<dd class="name">金牛座</dd></dl>
+		<dl class="ft_info2 fr">
+			<dd class="item1">运势
+				<span class="date">(2018-07-12)</span></dd>
+			<dd class="item2 starbox" data="3">
+				<span class="star"></span>
+			</dd>
+			<dd class="item3">顺其自然一点</dd></dl>
+	</div>
+	<ul class="aspect clear">
+		<li>
+			<span>爱情运势</span>
+			<div class="starbox" data="3">
+				<span class="star"></span>
+			</div>
+		</li>
+		<li>
+			<span>事业学业</span>
+			<div class="starbox" data="3">
+				<span class="star"></span>
+			</div>
+		</li>
+		<li>
+			<span>财富运势</span>
+			<div class="starbox" data="3">
+				<span class="star"></span>
+			</div>
+		</li>
+		<li>
+			<span>健康指数</span>90%</li>
+		<li>
+			<span>商谈指数</span>85%</li>
+		<li>
+			<span>幸运颜色</span>紫色</li>
+		<li>
+			<span>幸运数字</span>4</li>
+		<li>
+			<span>速配星座</span>天秤座</li></ul>
+	<p class="line curr2"></p>
+	<dl class="desc">
+		<dt>综合运势</dt>
+		<dd>今天的你非常看重付出与回报的问题，心中有种不平衡感。对方对你产生厌恶感，而你的强烈占有欲是导致你感情不顺的原因。很多时候都显得比较烦躁，被方方面面的事情影响着。</dd>
+		<dt>爱情运势</dt>
+		<dd>恋人之间的言谈也要注意，不要伤害对方的心。</dd>
+		<dt>事业学业</dt>
+		<dd>要尊重同事，不要嘲笑表现差的同伴，这是没教养的表现。</dd>
+		<dt>财富运势</dt>
+		<dd>有求财的机会就要极力去争取，才能得到相应回报。</dd>
+		<dt>健康运势</dt>
+		<dd>平时注意少吃热气、辣的食物，很容易长痘痘。</dd></dl>
+</div>"])
