13aca1c995dfdf6e80901c588ddb98cd37296d89
beanLau <522405475@qq.com>
2018-07-20 11:17:39 +0800

    添加异步接口地址

--- sites/wangxiao.cn/common/base.js
+++ sites/wangxiao.cn/common/base.js
@@ -6,7 +6,9 @@ export default ({
     getProductSubjects: zdyxhttp + 'course/getProductSubjects',
     sendMessage: zdyxhttp + 'baiduUser/sendMessage',
     compareMessageCode: zdyxhttp + 'baiduUser/compareMessageCode',
-    getGoodsCatalog: zdyxhttp + 'Order/getProductData'
+    getGoodsCatalog: zdyxhttp + 'Order/getProductData',
+    getOrderList: zdyxhttp + 'Order/orderList',
+    cancelOrder: zdyxhttp + 'Order/cancelOrder'
   },
   getQueryString: function (name) {
     let reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i')
