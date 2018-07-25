5f585057dd5d0f78da4f5c3add12f8d835a2a166
kyf <kyf456@163.com>
2018-07-25 17:58:52 +0800

    debug

--- sites/mip.6renyou.com/components/mip-dest-content/mip-dest-content.vue
+++ sites/mip.6renyou.com/components/mip-dest-content/mip-dest-content.vue
@@ -3,65 +3,75 @@
 </style>
 
 <template>
-        <div class="dest-content">
-        	<div class="dest-box">
-        		<ul class="dest-list clearfix">
-        			<li class="list" v-for="item in destination">
-        				<a :href="host + '/dest/' + item.name" mip-link>
-	        				<div class="dest-img">
-	        					<mip-img :src="item.img" />
-	        				</div>
-	        				<span class="dest-name">{{item.name}}</span>
-        				</a>
-        			</li>
-        		</ul>
-        	</div>
-        </div>
+  <div class="dest-content">
+    <div class="dest-box">
+      <ul class="dest-list clearfix">
+        <li
+          v-for="(item, index) in destination"
+          :key="index"
+          class="list">
+          <a
+            :href="host + '/dest/' + item.name"
+            mip-link>
+            <div class="dest-img">
+              <mip-img :src="item.img" />
+            </div>
+            <span class="dest-name">{{ item.name }}</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
 
 </template>
 
 <script>
-    import img_na from '@/static/img/index/dest-na.jpg';
-    import img_sa from '@/static/img/index/dest-sa.jpg';
-    import img_europe from '@/static/img/index/dest-europe.jpg';
-    import img_australia from '@/static/img/index/dest-australia.jpg';
-    import img_thailand from '@/static/img/index/dest-thailand.jpg';
-    import img_singapore from '@/static/img/index/dest-singapore.jpg';
-    import img_japan from '@/static/img/index/dest-japan.jpg';
-    import img_taiwan from '@/static/img/index/dest-taiwan.jpg';
+import imgNa from '@/static/img/index/dest-na.jpg'
+import imgSa from '@/static/img/index/dest-sa.jpg'
+import imgEurope from '@/static/img/index/dest-europe.jpg'
+import imgAustralia from '@/static/img/index/dest-australia.jpg'
+import imgThailand from '@/static/img/index/dest-thailand.jpg'
+import imgSingapore from '@/static/img/index/dest-singapore.jpg'
+import imgJapan from '@/static/img/index/dest-japan.jpg'
+import imgTaiwan from '@/static/img/index/dest-taiwan.jpg'
 
-	export default {
-        props: ['host'],
-		data () {
-			return {
-                destination: [{
-                    name: '北美',
-                    img: img_na,
-                },{
-                    name: '南美',
-                    img: img_sa,
-                },{
-                    name: '欧洲',
-                    img: img_europe,
-                },{
-                    name: '澳洲',
-                    img: img_australia,
-                },{
-                    name: '泰国',
-                    img: img_thailand,
-                },{
-                    name: '新加坡',
-                    img: img_singapore,
-                },{
-                    name: '日本',
-                    img: img_japan,
-                },{
-                    name: '台湾',
-                    img: img_taiwan,
-                }],
-            };	
-		},
-		methods: {
-		}
-	};
+export default {
+  props: {
+    'host': {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+      destination: [{
+        name: '北美',
+        img: imgNa
+      }, {
+        name: '南美',
+        img: imgSa
+      }, {
+        name: '欧洲',
+        img: imgEurope
+      }, {
+        name: '澳洲',
+        img: imgAustralia
+      }, {
+        name: '泰国',
+        img: imgThailand
+      }, {
+        name: '新加坡',
+        img: imgSingapore
+      }, {
+        name: '日本',
+        img: imgJapan
+      }, {
+        name: '台湾',
+        img: imgTaiwan
+      }]
+    }
+  },
+  methods: {
+  }
+}
 </script>
--- sites/mip.6renyou.com/components/mip-submit-button/mip-submit-button.vue
+++ sites/mip.6renyou.com/components/mip-submit-button/mip-submit-button.vue
@@ -3,19 +3,35 @@
 </style>
 
 <template>
-    <div class="six-ft-bar six-ft-btn">
-        <a mip-link :href="'/order/submit/' + dest + '/' + days + '?tripid=' + tripid" class="ft-btn-submit">咨询旅行顾问</a>
-    </div>
+  <div class="six-ft-bar six-ft-btn">
+    <a
+      :href="'/order/submit/' + dest + '/' + days + '?tripid=' + tripid"
+      mip-link
+      class="ft-btn-submit">咨询旅行顾问</a>
+  </div>
 </template>
 
 <script>
-	export default {
-		data () {
-			return {};	
-		},
-        props: ['dest', 'days', 'tripid'],
-		methods: {
-			
-		}
-	};
+export default {
+  props: {
+    'dest': {
+      type: String,
+      required: true
+    },
+    'days': {
+      type: Number,
+      required: true
+    },
+    'tripid': {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {}
+  },
+  methods: {
+
+  }
+}
 </script>
