ae9579dd0b060e7df0c2c1f636ac1147b2dcef2a
rongxiang <rongxiang@kuaifawu.com>
2018-07-26 16:08:31 +0800

    提交config

--- sites/kuaifawu.com/utils/config.js
+++ sites/kuaifawu.com/utils/config.js
@@ -1,7 +1,8 @@
 export default {
   data () {
     return {
-      apiurl: 'https://api.kuaifawu.com/mip/'
+      apiurl: 'https://api.kuaifawu.com/mip/',
+      burl:'http://b.t.kuaifawu.com'
     }
   }
 }
