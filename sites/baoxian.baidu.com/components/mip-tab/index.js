538d122f10199633a4fdc5af1ae8bbacdecc1c27
weizhongjian <weizhongjian@baidu.com>
2018-07-26 18:10:23 +0800

    baoxian.baidu.com

---
+++ sites/baoxian.baidu.com/components/mip-tab/index.js
@@ -0,0 +1,8 @@
+/**
+ * @file tab组件
+ */
+
+import Tab from './tab.vue';
+import TabPanel from './tab-panel.vue';
+import OriginTab from './origin-tab.vue';
+export {Tab, TabPanel, OriginTab};
---
+++ sites/baoxian.baidu.com/components/mip-tab/tab-panel.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="panel" v-show="isShow">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    name: "c-tab-panel",
+    props: {
+        tabname: String,
+        tabdata: String
+    },
+    data () {
+        return {
+        }
+    },
+    mounted () {
+        this.$parent.addPanes(this);
+    },
+    computed: {
+        isShow () {
+            return this.$parent.current == this.tabname;
+        }
+    },
+    methods: {
+    }
+}
+</script>
