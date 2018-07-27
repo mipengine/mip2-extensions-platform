12943654acf70a5cf8ffaab6f2b2736599e456d4
XiaodaoLin <834813402@qq.com>
2018-07-27 15:06:26 +0800

    结果展示修复

--- sites/wage.com/components/mip-result/mip-result.vue
+++ sites/wage.com/components/mip-result/mip-result.vue
@@ -11,7 +11,7 @@
       <div class="h43" v-show="hasAccumulation"><span>公积金个人缴纳</span><span class="right">{{resultdata.accumulation}}元</span></div>
     </div>
     <div class="result-div mt10">
-      <div class="my-wage-detail2 h43" @click="closeWageDetail" :class="{'no-bor':isTrue,'my-wage-detail':isTrue}">我的工资计算详情</div>
+      <div class="my-wage-detail2 h43" @click="closeWageDetail" :class="{'no-bor':!isTrue,'my-wage-detail':isTrue}">我的工资计算详情</div>
       <section v-show="isShow">
         <div class="pd16">
           <p class="mb10"><span>{{resultdata.tag==1?afterTitle:beforeTitle}}</span><span class="right">{{resultdata.tag==1?resultdata.realIncome:resultdata.wage}}元</span></p>
@@ -34,6 +34,153 @@
   </div>
 </template>
 <style scoped>
+.wrapper {
+    position: relative;
+    margin: 0 15px 10px 15px;
+    text-align: center;
+    border-radius: 5px;
+    background: #fff;
+    border: 1px solid #E6E4E1;
+}
+
+.wrapper>div {
+    height: 45px;
+    line-height: 45px;
+    margin: 0 16px;
+    font-size: 16px;
+    text-align: left;
+    color: #999;
+    border-bottom: 1px solid #F5F5F5;
+}
+
+.wrapper label {
+    width: 10em;
+    font-family: PingFang-SC-Medium;
+    font-size: 13px;
+    font-weight: 500;
+    color: #000;
+}
+
+label {
+    position: absolute;
+    width: 7em;
+    font-size: 16px;
+}
+
+input {
+    width: 100%;
+    padding-left: 7em;
+    text-align: right;
+    box-sizing: border-box;
+    border: none;
+    outline: none;
+}
+
+input::placeholder {
+    text-align: right;
+    color: #ccc;
+}
+
+.arrow {
+    padding-right: 15px;
+    background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAB+klEQVRIS62Wv4sTQRTHv28DdoJ98ldok8ybNHdcd4o22vnjCkUOEeWw9AeKIocgHlYqFuIV8UepnRbZmVVBG0mllZWiRVJ4guB+5UmuS7KzZ6Yadt9+Pvv2vZlZwZQRQrhKcst7f2NaTMp1mRTU7/f3ZVn2FkADwFlVvZMCmxQzUWCBMcYVkvdtTvKk9/7fvO6YKjBQCOEMAHv7PyJy1Dm3OVfBWHIBwE2TkDzsvX9eRzIzg21QjPEKyYsAfpM86L1/mSpJEowzuQXgPIAtksve+9cpkmQBSYkx3gVwWkR+klxS1aJKkiwYd5MURfGQ5DEAoyzLFjqdzodZkloCA/V6vUar1dq0ggP4LiKLzrmP0yS1BQYaDAa7RqPRU5L7SX7NsqzrnPtca6FVfds8z3eLyBcAewC8UdXO3ASWwXA4fALgAIBvjUaj2263P81FYDVoNpuPARwB8ENEFuZWg3GrPgBwwrqoLMvFbrf7fi5dZPAQwoaIrNpiE5El51ysqlVyF+V5vi4iawB+AVhW1VdVcLufJAghXAZwabwXHfLev0iBJwlijGsk1203tcKq6rNUeKUgxrhKcsPOHADHVfVRHfhMQQjBOsU6xsYpVb1XFz5VUBTF3rIs39mZLCLnnHO3dwKfmUGM8br9VajqtZ3CK2vwP+DtZ/8CVbfLGdhcLzIAAAAASUVORK5CYII=) no-repeat 100% 56%;
+    background-size: 10px;
+}
+
+.ip-disable {
+    pointer-events: none;    
+}
+
+.onOff {
+    position: absolute;
+    right: 19px;
+    top: 15px;
+    height: 20px;
+    width: 38px;
+    background: #bbb;
+    border-radius: 10px;
+    cursor: pointer;
+    display: block;
+}
+
+.onOff_off {
+    position: absolute;
+    top: 1px;
+    left: 1px;
+    width: 18px;
+    height: 18px;
+    background: #fff;
+    border-radius: 50%;
+}
+
+.open {
+    background: #FF8D1D;
+}
+
+.none {
+    display: none;
+}
+
+.onOff_off.active {
+    left: 50%;
+}
+
+.calculateBtn {
+    width: 50%;
+    height: 45px;
+    line-height: 45px;
+    margin: 20px auto;
+    font-size: 17px;
+    color: #fff;
+    font-weight: 500;
+    text-align: center;
+    border-radius: 45px;
+    background: -webkit-linear-gradient(left, #FF731F, #FFA53A);
+    /* Safari 5.1 - 6.0 */
+    background: -o-linear-gradient(right, #FF731F, #FFA53A);
+    /* Opera 11.1 - 12.0 */
+    background: -moz-linear-gradient(right, #FF731F, #FFA53A);
+    /* Firefox 3.6 - 15 */
+    background: linear-gradient(to right, #FF731F, #FFA53A);
+    /* 标准的语法 */
+}
+
+.tips {
+    position: relative;
+    bottom: 0;
+    width: 100%;
+    height: 40px;
+    line-height: 40px;
+    font-size: 12px;
+    color: #ACA9A7;
+    text-align: center;
+}
+
+.mb21 {
+    margin-bottom: 21px;
+}
+
+.right {
+    float: right;
+}
+
+li {
+    list-style: none;
+}
+
+.mt20 {
+    margin-top: 20px;
+}
+
+.left {
+    float: left;
+}
+.cfont{
+    color:#FF8D1D;
+}
+.c-disable{
+    color:#ACA9A7;
+}
 .wrapper2 {
   margin: 0 auto !important;
   text-align: center;
