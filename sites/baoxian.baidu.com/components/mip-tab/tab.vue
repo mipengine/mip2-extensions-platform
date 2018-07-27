ab68b84feb859a3f5692b7de3f45edfb5afd0666
zhuguoxi <zhuguoxi@baidu.com>
2018-07-27 13:40:19 +0800

    rm baoxian

--- sites/baoxian.baidu.com/components/mip-tab/tab.vue
+++
@@ -1,9 +0,0 @@
-<script>
-import OriginTab from './origin-tab.vue';
-export default {
-    name: 'c-tab',
-    componentName: 'c-tab',
-    extends: OriginTab
-}
-</script>
-<style lang="less" src="./tab.less">
