61418a95b617463d4d6b3cc1b42c877ed79f10fd
Qi Ming <phoenixqm@gmail.com>
2018-07-28 19:55:59 +0800

    菩提果科技修复回退刷新等bug (#101)
    * mutiple fix
    
    * fix style
    
    * add fixed init
    
    * update contract & feedback
    
    * bug fix
    
    * add show-page hook

--- sites/mip.putibaby.com/components/mip-ptg-select-master/mip-ptg-select-master.vue
+++ sites/mip.putibaby.com/components/mip-ptg-select-master/mip-ptg-select-master.vue
@@ -1656,6 +1656,7 @@ function parseJSON (response) {
 
 API.wrapRet_ = function (api, opts, fn) {
   console.log('posting to ' + api)
+  opts.mip_sid = API.sessionId || ''
   fetch(api, {
     method: 'POST',
     credentials: 'include',
@@ -1678,6 +1679,14 @@ API.getSelectMaster = function (filter, fn) {
   API.wrapRet_(
     'https://mip.putibaby.com/api/get_select_master', {
       'filter': filter
+    },
+    fn)
+}
+
+API.checkUnionAgain = function (opt, fn) {
+  API.wrapRet_(
+    'https://mip.putibaby.com/api/check_union_again', {
+      'opt': opt
     },
     fn)
 }
@@ -1794,6 +1803,27 @@ export default {
       console.log(str)
     })
 
+    window.addEventListener('show-page', () => {
+      console.log('show-page')
+      if (self.isUnion || !self.isLogin) {
+        return
+      }
+      API.checkUnionAgain('', function (isOk, res) {
+        if (isOk) {
+          console.log(res)
+          self.isLogin = res.isLogin
+          self.isUnion = res.isUnion
+          // MIP.setData({'#isLogin': true})
+          // MIP.setData({'#isUnion': event.userInfo.isUnion})
+        } else {
+          console.log(res)
+        }
+      })
+    })
+    window.addEventListener('hide-page', () => {
+
+    })
+
     this.$element.customElement.addEventAction('logindone', event => {
       // 这里可以输出登录之后的数据
 
@@ -1804,6 +1834,9 @@ export default {
       // self.$set(self, 'isUnion', event.userInfo.isUnion)
       self.isLogin = true
       self.isUnion = event.userInfo.isUnion
+      // MIP.setData({'#isLogin': true})
+      // MIP.setData({'#isUnion': event.userInfo.isUnion})
+
       var origin = API.next_cmd || event.origin
       // origin = origin || sessionStorage.next_cmd || localStorage.getItem('origin')
 
