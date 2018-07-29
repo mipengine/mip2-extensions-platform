a394bd0b20aa8105fa01c2b93e60d4c0d1be6d98
XiaodaoLin <834813402@qq.com>
2018-07-29 16:45:09 +0800

    修改规范错误13

--- sites/wage.com/components/mip-social-security/mip-social-security.vue
+++ sites/wage.com/components/mip-social-security/mip-social-security.vue
@@ -282,8 +282,9 @@ export default {
           }
       }
     },
-    showValues: function () {
-      if (this.isshow) { // 公积金
+    showValues:{
+      get: function () {
+        if (this.isshow) { // 公积金
         if (this.tag === 1) { // 税前
           this.selectValue = '按照工资'
           this.fromWage = true
@@ -320,6 +321,7 @@ export default {
             '按照最低标准'
           ]
         }
+      }
       }
     }
   },
