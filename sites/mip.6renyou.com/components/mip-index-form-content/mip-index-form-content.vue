a0271b0308400c7cf4507f149cd7f0d70d82d7dd
kyf <kyf456@163.com>
2018-07-31 19:27:54 +0800

    debug

--- sites/mip.6renyou.com/components/mip-index-form-content/mip-index-form-content.vue
+++ sites/mip.6renyou.com/components/mip-index-form-content/mip-index-form-content.vue
@@ -96,7 +96,7 @@ export default {
         toast.show('您还未填写目的地', options)
         return
       }
-      MIP.viewer.open('/order/submit/' + this.destName + '/' + this.days)
+      MIP.viewer.open('./order/submit/' + this.destName + '/' + this.days)
     }
 
   }
