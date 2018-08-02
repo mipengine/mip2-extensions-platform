8e2ad07e2b855793bff2973aac607a2c3b4e9af9
Gump <u402918993@gmail.com>
2018-08-02 15:25:08 +0800

    delete .svn .log

--- sites/xmdcwbaidu.com/common/js/config.js
+++ sites/xmdcwbaidu.com/common/js/config.js
@@ -3,6 +3,6 @@ export const gConst = {
   BASE_URL_PAGE: 'https://jmt.sssvip4.natapp.cc/xmdcwbaidu.com/example/'
 
   // BASE_URL: 'http://test.xx-motor.com/yzcw-web-admin/',
-  // BASE_URL_PAGE: 'http://127.0.0.1:8111/example/',
-  // TEST: 'test'
+  // BASE_URL_PAGE: 'http://127.0.0.1:8111/example/'
+
 }
