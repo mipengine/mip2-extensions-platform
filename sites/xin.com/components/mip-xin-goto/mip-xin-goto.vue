7abc3ecfeeefec9d8607bac87ea1c933674db121
yumengkang <yumengkang@xin.com>
2018-08-11 18:52:42 +0800

    增加优信二手车需要的各种组件

---
+++ sites/xin.com/components/mip-xin-goto/mip-xin-goto.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="look-params">查看详细参数配置
+    <span class="params-arrow"></span>
+  </div>
+</template>
+
+<style scoped>
+.look-params {
+  border-top: 0.5px solid #eee;
+  height: 1rem;
+  line-height: 1rem;
+  font-size: 0.26rem;
+  color: #585858;
+  text-align: center;
+}
+.params-arrow {
+  display: inline-block;
+  width: 0.14rem;
+	height: 0.14rem;
+	-webkit-transform: rotate(-315deg);
+	transform: rotate(-315deg);
+	border: solid #dbdada;
+  border-width: 2px 2px 0 0;
+}
+</style>
+<script>
+import base from '../../common/utils/base';
+import {requestFun} from  '../../common/utils/reqUtils';
+
+export default {
+   data(){
+      return{
+       
+      }
+    },
+    created () {
+      base.setMediaBase()
+    },
+    mounted(){
+     
+    },
+    methods:{
+      
+    }
+}
+</script>
---
+++ sites/xin.com/components/mip-xin-price/mip-xin-price.vue
@@ -0,0 +1,710 @@
+<template>
+  <div>
+    <div class="car-infor car-infor-bottom">
+      <div class='car-title'>
+        <span class="car-desc">{{carInfo.carname}}</span>
+      </div>
+      <div class='price-info '>
+        <div class="car-price-content" v-if="carInfo.panel_price != 0">
+          <span class="car-price">{{carInfo.panel_price}}</span>
+          <span class='car-price-unit' v-if="carInfo.panel_price">万</span>
+          <!-- <img class="car-price-trend" src="https://s1.xinstatic.com/static/newcar-m/imgs/car-detail/price-trend_c4e8f6d.png" /> -->
+          <div class="beyond-value-box"  v-if="beyondUrl" @click="beyondCompare">
+            <mip-img class="car-price-trend"   src="https://s1.xinstatic.com/static/newcar-m/imgs/car-detail/price-trend_c4e8f6d.png" />            
+            <span class="beyond-value" >车价超值</span>
+            <div  class="next-page next-page-overcolor"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class='spokes-person-price' :class="{'spokes-person-pricebottom' : showSimilarmodule || showMessage}">
+      <div class="open-fanice" v-if="carInfo.show_price && carInfo.monthly_price">
+        <span class='pay-detail'  v-if="carInfo.is_direct_rent_car == 1"> 一成首付{{carInfo.show_price}} 月供{{carInfo.monthly_price}} </span>
+        <span class='pay-detail' v-if="carInfo.is_direct_rent_car == 0"> 首付{{carInfo.show_price}} 月供{{carInfo.monthly_price}} </span>
+        <div class="car-fanice-see" @click="openFance">
+          <span>查看详细金融方案</span>
+          <div class="next-page-color next-page "></div>
+        </div>
+      </div>
+      <div class="open-car-allmessagebox" v-if="!showMessage && carInfo.panel_price && showSimilarmodule" @click="openMessage">
+        <div class="open-car-allmessage">
+          <div>展开全部车辆信息</div>
+          <div class="show-carmessage-icon">
+            <div class="open-message-icon1"></div>
+            <div class="open-message-icon2" style="margin-top: -2px;"></div>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+
+    <div class="car-params" v-if="showMessage || !showSimilarmodule">
+      <div class="bdline">
+        <div class="car-params-title">
+          <span class='car-file'>车辆档案</span>
+          <!-- <span class="car-shelf-time">今日上架</span> -->
+        </div>
+      </div>
+      <div class="driveInfo">
+        <div class="drive-info-list">
+          <div class="driveList drive-width">
+            <div>
+              <span class="driveKey">{{regist_date.title}}</span>
+            </div>
+            <div class="driveValue">
+              <span>{{regist_date.value}}</span>
+            </div>
+          </div>
+          <div class="driveList drive-width">
+            <div>
+              <span class="driveKey">{{mileage.title || '表显里程'}}</span>
+            </div>
+            <div class="driveValue">
+              <span>{{mileage.value}}</span>
+            </div>
+          </div>
+          <div class="driveList get-car">
+            <div>
+              <span class="driveKey">{{emission.title || "排放标准"}}</span>
+            </div>
+            <div class="driveValue">
+              <span>{{emission.value}}</span>
+            </div>
+          </div>
+        </div>
+        <div class="drive-info-list drive-list-bottom">
+          <div class="driveList drive-width">
+            <!-- <div>
+                        <span class="driveKey">上架时间</span >
+                    </div>
+                    <div class="driveValue">
+                        <span>2017.05.16</span >
+                    </div> -->
+            <div>
+              <span class="driveKey">{{color.title || "颜色"}}</span>
+            </div>
+            <div  class="driveValue">
+              <span>{{color.value}}</span>
+            </div>
+          </div>
+          <div class="driveList drive-width ">
+            <div>
+              <span class="driveKey">{{engine_type.title || "涡轮增压"}}</span>
+            </div>
+            <div class="driveValue">
+              <span>{{engine_type.value}}</span>
+            </div>
+          </div>
+          <!-- <div class="driveList get-car">
+            <div>
+              <span class="driveKey">所在城市</span>
+            </div>
+            <div class="driveValue">
+              <span>{{北京}}</span>
+            </div>
+          </div> -->
+          <div class="driveList get-car" v-if="lift_time.title && lift_time.value">
+                    <div>
+                        <span class="driveKey">{{lift_time.title}}</span >
+                    </div>
+                    <div class="driveValue">
+                        <span >{{lift_time.value}}</span >
+                    </div>
+                </div>
+        </div>
+      </div>
+      <div class='car-set'>
+        <div class="car-set-content" v-for="(item, index) in needCcid" :key="index"  v-if='index < 4'>
+          <div class="paramsInfo car-set-config" @click="openConfig(item.jump_page)">
+            <mip-img class="paramsIcon" :src="imageTitle + item.u_icon" />
+            <div class="car-highlight">
+              <span class="paramName">{{item.ccname}}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <mip-xin-config :url="url" :carid="carid" params="查看详细参数配置" :im-url="imUrl"></mip-xin-config>
+    </div>
+  </div>
+</template>
+<script>
+import { requestFun } from "../../common/utils/reqUtils";
+import { viewPoint, clickPoint } from "../../common/utils/stastic.js";
+import config from "../../common/utils/config";
+import {
+  setLocalStorage,
+  getLocalStorage,
+  getCarId,
+  getCityId
+} from "../../common/utils/utils.js";
+const pid = "/pages/detail";
+export default {
+  props: ["carInfo"],
+  data() {
+    return {
+      carInfo: {}, //车辆基本信息
+      carid: "",
+      cityid: "",
+      docs: {}, //车辆档案
+      needCcid: [], //车辆配置
+      color: {},
+      emission: {},
+      engine_type: {},
+      lift_time: {},
+      mileage: {},
+      regist_date: {},
+      imageTitle: "https:", //图片地址头
+      url: "",
+      isDirect: 0, //直购车标志
+      beyondUrl: "", //超值链接
+      imUrl: "" //M站的IM客服路径
+    };
+  },
+  props: [
+    "showMessage",
+    "carPrice",
+    "ename",
+    "showSimilarmodule",
+    "cityMessage"
+  ],
+  mounted() {
+    let param = {
+      carid: getCarId(),
+      cityid: getCityId()
+    };
+    requestFun("/ajax/car", {
+      method: "POST",
+      param: param
+    })
+      .then(res => {
+        if (res.car_info) {
+          this.carInfo = res.car_info;
+          this.isDirect = res.car_info.is_zg_car;
+          setLocalStorage('isDirect',JSON.stringify(res.car_info.is_zg_car));
+          let url = `/c/config_${getCarId()}.html${getLocalStorage(
+            "locationUrl"
+          )}`;
+          this.url = url;
+          this.beyondUrl = res.car_info.super_url;
+          this.imUrl = res.im_url;
+          this.$emit("showPrice", this.carInfo);
+          this.$emit("getImurl", res.im_url);
+        }
+        if (res.docs) {
+          this.docs = res.docs;
+          this.color = this.docs.color;
+          this.emission = this.docs.emission;
+          this.engine_type = this.docs.engine_type;
+          this.lift_time = this.docs.lift_time;
+          this.mileage = this.docs.mileage;
+          this.regist_date = this.docs.regist_date;
+        }
+        if (res.need_ccid) {
+          this.needCcid = res.need_ccid;
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  },
+  created() {},
+  methods: {
+    openConfig(url) {
+      MIP.viewer.open(url + getLocalStorage("locationUrl"), {
+        isMipLink: true
+      });
+    },
+    openParmas() {
+      clickPoint(
+        "collocation_more",
+        {
+          carid: getCarId()
+        },
+        () => {
+          MIP.viewer.open(this.url, {
+            isMipLink: true
+          });
+        },
+        {
+          pid: pid
+        }
+      );
+    },
+    openFance() {
+      let opurl = getLocalStorage("locationUrl")
+        ? getLocalStorage("locationUrl") + "&"
+        : "?";
+      MIP.viewer.open(
+        `${
+          config.mHost
+        }/halfprice/detail/${getCarId()}/${opurl}cityid=${getCityId()}&ename=${
+          JSON.parse(this.cityMessage).ename
+        }`,
+        {
+          isMipLink: true
+        }
+      );
+      clickPoint(
+        "halfplan_detail",
+        {
+          carid: getCarId(),
+          icon: this.carInfo.is_direct_rent_car
+        },
+        null,
+        {
+          pid: pid
+        }
+      );
+    },
+    openMessage() {
+      this.$emit("showAllMessage", '{ param: "333" }');
+      console.log(getCarId(), this.isDirect);
+      clickPoint(
+        "open_config_detail",
+        {
+          carid: getCarId(),
+          type: this.isDirect
+        },
+        null,
+        {
+          pid: pid
+        }
+      );
+    },
+    beyondCompare() {
+      MIP.viewer.open(this.beyondUrl + `${getLocalStorage("locationUrl")}`, {
+        isMipLink: true
+      });
+    }
+  },
+  watch: {
+    carInfo(newval, oldval) {
+      if (typeof newval == "string") {
+        this.carInfo = JSON.parse(newval);
+      }
+    }
+  }
+};
+</script>
+<style scoped>
+.car-infor {
+  display: flex;
+  flex-direction: column;
+}
+
+.car-infor-bottom {
+  margin-bottom: 0.15rem;
+}
+
+.car-title {
+  width: 6.7rem;
+  margin-left: 0.4rem;
+  line-height: 0.3rem;
+  padding-top: 0.68rem;
+  min-height: 0.36rem;
+}
+
+.car-desc {
+  font-size: 0.36rem;
+  font-weight: bold;
+  color: #3a4144;
+  line-height: 0.6rem;
+}
+
+.price-info {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-end;
+  padding-left: 0.4rem;
+  padding-right: 0.4rem;
+}
+
+.car-price-desc {
+  display: flex;
+  flex-direction: column;
+}
+
+.car-price-box {
+  display: flex;
+  flex-direction: column;
+}
+
+.car-price-content {
+  display: flex;
+  align-items: flex-end;
+}
+
+.car-price {
+  font-size: 0.6rem;
+  color: #f85d00;
+  font-weight: bold;
+  margin-top: 0.26rem;
+  margin-bottom: -0.15rem;
+  min-height: 0.6rem;
+  /* background-color: #f85d00 */
+}
+
+.car-price-unit {
+  font-size: 0.26rem;
+  color: #f85d00;
+}
+
+.car-price-trend {
+  width: 0.24rem;
+  height: 0.28rem;
+  margin-left: 0.12rem;
+  margin-right: 0.12rem;
+  /* margin-top: -0.05rem; */
+}
+/* .car-price-trend img {
+  margin-top: -0.04rem;
+} */
+.new-car-tax {
+  align-items: flex-end;
+  padding-left: 0.4rem;
+  padding-top: 0.2rem;
+}
+
+.new-car-taxbottom {
+  margin-top: 0.1rem;
+}
+
+.car-tax-font {
+  font-size: 0.26rem;
+  font-family: PingFangSC-Regular;
+  color: rgba(88, 88, 88, 1);
+  margin-top: 0.24rem;
+  margin-left: 0.4rem;
+}
+
+.beyondValue {
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+}
+.beyond-value-box {
+  display: flex;
+  align-items: center;
+}
+.beyond-value {
+  text-align: center;
+  border-radius: 4px;
+  font-size: 0.26rem;
+  font-family: PingFangSC-Regular;
+  color: rgba(248, 93, 0, 1);
+}
+
+.car-price-discount-box {
+  display: flex;
+  align-items: flex-end;
+  margin-right: 0.1rem;
+  margin-top: 0.24rem;
+}
+
+.car-price-discount {
+  border: 1px solid #f85d00;
+  border-radius: 2px;
+  padding: 6px;
+  font-size: 0.28rem;
+  color: #f85d00;
+  text-align: center;
+  width: 1.26rem;
+  white-space: nowrap;
+  /* background-color: #f85d00; */
+}
+
+.car-price-bottom {
+  font-size: 0.28rem;
+  color: #f85d00;
+  text-align: center;
+}
+
+.spokes-person-price {
+  display: flex;
+  margin-top: 0.24rem;
+  flex-direction: column;
+}
+.spokes-person-pricebottom {
+  border-bottom: 0.18rem solid rgba(241, 241, 241, 1);
+}
+.open-fanice {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  /* height: 1rem; */
+  padding-left: 0.4rem;
+  padding-right: 0.4rem;
+  padding-bottom: 0.3rem;
+}
+
+.car-fanice-see {
+  display: flex;
+  align-items: center;
+  font-size: 0.26rem;
+  font-family: PingFangSC-Regular;
+  color: rgba(88, 88, 88, 1);
+}
+
+.next-page {
+  width: 0.14rem;
+  height: 0.14rem;
+  line-height: 1rem;
+  transform: rotate(-315deg);
+  margin-top: 0.03rem;
+}
+
+.next-page-color {
+  border: solid rgba(88, 88, 88, 1);
+  border-width: 1px 1px 0 0;
+}
+
+.next-page-overcolor {
+  border: solid rgb(248, 93, 0);
+  border-width: 1px 1px 0 0;
+}
+
+.open-car-allmessage {
+  text-align: center;
+  line-height: 0.7rem;
+  width: 6.7rem;
+  height: 0.7rem;
+  background: rgba(255, 255, 255, 1);
+  border-radius: 0.06rem;
+  border: 2px solid rgba(248, 93, 0, 1);
+  font-size: 0.28rem;
+  font-family: PingFangSC-Regular;
+  color: rgba(248, 93, 0, 1);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.open-car-allmessagebox {
+  padding-left: 0.4rem;
+  padding-right: 0.4rem;
+  margin-bottom: 0.2rem;
+}
+
+.spokes-person {
+  width: 1rem;
+  height: 1.4rem;
+  line-height: 1.45rem;
+  margin-top: -0.48rem;
+  position: absolute;
+  margin-left: 0.4rem;
+}
+
+.pay-detail {
+  font-size: 0.26rem;
+  font-family: PingFangSC-Regular;
+  color: rgba(88, 88, 88, 1);
+}
+
+.car-file {
+  margin-left: 0.4rem;
+  font-size: 0.34rem;
+  color: #333333;
+  font-weight: bold;
+}
+
+.car-params {
+  display: flex;
+  flex-direction: column;
+}
+
+.bdline {
+  display: flex;
+  flex-direction: column;
+}
+
+.boundary-box {
+  width: 100%;
+  height: 0.2rem;
+  background-color: #f9f9f9;
+}
+
+.car-params-title {
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid rgba(241, 241, 241, 1);
+  align-items: center;
+  height: 1.1rem;
+  border-left: 0.04rem solid rgba(248, 93, 0, 1);
+}
+
+.car-shelf-time {
+  color: #848484;
+  font-size: 0.26rem;
+  margin-right: 0.4rem;
+}
+
+.driveInfo {
+  display: flex;
+  flex-wrap: wrap;
+  padding-left: 0.4rem;
+  justify-content: space-between;
+  margin-top: 0.6rem;
+  padding-right: 0.4rem;
+}
+
+.driveList {
+  display: flex;
+  flex-direction: column;
+  /* width: 33.3%; */
+  /* width: 210px;
+        margin-right: 25px; */
+}
+
+.drive-list-bottom {
+  border-bottom: 1px solid rgba(241, 241, 241, 1);
+}
+
+.drive-info-list {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+}
+
+.driveKey {
+  font-size: 0.26rem;
+  padding-top: 0.5rem;
+  padding-bottom: 0.2rem;
+  font-family: PingFangSC-Regular;
+  color: rgba(153, 153, 153, 1);
+}
+
+.drive-width {
+  width: 2.2rem;
+}
+
+/* .naturally-air{
+        margin-left:50px;
+    } */
+
+.get-car {
+  white-space: nowrap;
+}
+
+.driveValue {
+  font-size: 0.32rem;
+  font-weight: bold;
+  margin-top: 0.2rem;
+  margin-bottom: 0.5rem;
+  font-family: PingFangSC-Medium;
+  color: rgba(27, 27, 27, 1);
+}
+
+.car-info-bottom {
+  height: 1px;
+  width: 100%;
+  background-color: #f1f1f1;
+  margin-top: 0.4rem;
+  margin-right: 0.4rem;
+  margin-left: 0.4rem;
+}
+
+.car-set {
+  display: flex;
+  padding-top: 0.6rem;
+}
+
+.car-set-content {
+  display: flex;
+  /* justify-content: space-between; */
+  margin-left: 0.4rem;
+}
+
+.u-certified-content {
+  margin-left: 0.4rem;
+}
+
+.paramsInfo {
+  display: flex;
+  flex-direction: column;
+}
+
+.params-info {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  /* margin-right: 60px; */
+}
+
+.car-set-config {
+  margin-bottom: 0.4rem;
+}
+
+.paramsIcon {
+  width: 0.56rem;
+  height: 0.56rem;
+  margin-left: 0.4rem;
+}
+
+.paramName {
+  text-align: center;
+  width: 100%;
+  font-size: 0.26rem;
+  font-family: PingFangSC-Regular;
+  color: rgba(88, 88, 88, 1);
+}
+
+.car-set-bottom {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-top: 1px solid rgba(241, 241, 241, 1);
+  padding-bottom: 0.37rem;
+  padding-top: 0.37rem;
+  border-bottom: 0.18rem solid rgba(241, 241, 241, 1);
+}
+
+.car-highlight {
+  width: 1.3rem;
+  text-align: center;
+}
+
+.lookParams {
+  font-size: 0.26rem;
+  text-align: center;
+  margin-left: 0.04rem;
+  font-family: PingFangSC-Regular;
+  color: rgba(88, 88, 88, 1);
+}
+
+.paramsArrow {
+  width: 0.14rem;
+  height: 0.14rem;
+  transform: rotate(-315deg);
+  border: solid #dbdada;
+  border-width: 2px 2px 0 0;
+}
+
+.open-message-icon1 {
+  width: 0.12rem;
+  height: 0.12rem;
+  line-height: 1rem;
+  -webkit-transform: rotate(-315deg);
+  -ms-transform: rotate(-315deg);
+  transform: rotate(-225deg);
+  margin-top: 0.03rem;
+  border: solid rgb(255, 142, 75);
+  border-width: 1px 1px 0 0;
+}
+.open-message-icon2 {
+  width: 0.12rem;
+  height: 0.12rem;
+  line-height: 1rem;
+  -webkit-transform: rotate(-315deg);
+  -ms-transform: rotate(-315deg);
+  transform: rotate(-225deg);
+  margin-top: 0.03rem;
+  border: solid rgb(248, 93, 0);
+  border-width: 1px 1px 0 0;
+}
+.show-carmessage-icon {
+  margin-top: -0.15rem;
+  margin-left: 0.15rem;
+}
+</style>
