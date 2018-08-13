eca37c6edc61f6b8478186ccefadb5646fa0d82d
changsi.chichangsi <changsi.chichangsi@alibaba-inc.com>
2018-08-13 12:05:34 +0800

    udapte code

--- sites/m.1688.com/components/mip-1688seo-list/mip-1688seo-list.vue
+++ sites/m.1688.com/components/mip-1688seo-list/mip-1688seo-list.vue
@@ -54,6 +54,7 @@
 <style scoped lang="less">
 </style>
 <script>
+
 import './mip-1688seo-list.less'
 export default {
   props: {
@@ -76,14 +77,15 @@ export default {
   mounted () {},
   methods: {
     next () {
-      let pageIndex = 2;
-      let _href = this.canonicalUrl || '';
+      let pageIndex = 2
+      let _href = this.canonicalUrl || ''
       if (_href.indexOf('?') !== -1) {
-        window.location.href = _href + '&beginPage=' + pageIndex;
+        window.location.href = _href + '&beginPage=' + pageIndex
       } else {
-        window.location.href = _href + '?beginPage=' + pageIndex;
+        window.location.href = _href + '?beginPage=' + pageIndex
       }
     }
   }
 }
+
 </script>
