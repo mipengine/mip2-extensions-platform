6a55fb130901d988b8535a3e707a58a9f8969faa
changsi.chichangsi <changsi.chichangsi@alibaba-inc.com>
2018-08-17 12:45:04 +0800

    udpatecode

--- sites/m.1688.com/components/mip-1688seo-pix/mip-1688seo-pix.vue
+++ sites/m.1688.com/components/mip-1688seo-pix/mip-1688seo-pix.vue
@@ -10,24 +10,28 @@ export default {
       type: String,
       required: true
     },
-    canonical:{
-      type:String,
-      required:true
+    canonical: {
+      type: String,
+      required: true
     },
-    title:{
-      type:String,
-      required:true
+    title: {
+      type: String,
+      required: true
     }
   },
-  mounted () {
-    let element = this.$element
-    let img = new Image()
-    img.style.display = 'none';
+  mounted() {
+    let element = this.$element;
+    let img = new Image();
+    img.style.display = "none";
     let canonical = this.canonical || "";
     let title = this.title || "";
-    let url ="https://log.mmstat.com/m.gif?spm-cnt=a262wv.11860230&aplus&canonical="+canonical+"&title="+title;
-    img.src = this.src
-    element.appendChild(img)
+    let url =
+      "https://log.mmstat.com/m.gif?spm-cnt=a262wv.11860230&aplus&canonical=" +
+      canonical +
+      "&title=" +
+      title;
+    img.src = this.src;
+    element.appendChild(img);
   }
-}
+};
 </script>
