1e30a9dd6628a3c2c0f01fe678fea41421b15d3c
chenqiushi <tracy909@126.com>
2018-08-23 15:21:22 +0800

    预览构建编译与上线编译的方式改成一致

---
+++ sites/mip.hulian.top/components/mip-hulian-par/mip-hulian-par.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+<mip-iframe
+    :src="greeting"
+    allowfullscreen
+    width="100%"
+    :height="height"
+    allowtransparency="true">
+</mip-iframe>
+</div>
+
+</template>
+
+<style scoped>
+    
+
+</style>
+
+<script>
+var hashstr = location.hash.replace('#','');
+var params = hashstr.split('&');
+var url = 'https://m.hulian.top/help/'+params[1].replace('type=','')+'/m/'+params[0].replace('id=','')+'.html'
+var height = MIP.viewport.getHeight('mip-iframe');
+export default {
+    // props: [],
+    computed: {
+      greeting() { 
+        return url;
+      },
+      height(){
+        return  height;
+      }
+    }
+  }
+</script>
