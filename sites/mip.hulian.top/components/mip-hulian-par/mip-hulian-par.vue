f6460ec7fc9eb2c8e382c806c5a59754de2f22b4
syjie <1211419561@qq.com>
2018-08-23 18:01:56 +0800

    Update mip-hulian-par.vue

--- sites/mip.hulian.top/components/mip-hulian-par/mip-hulian-par.vue
+++ sites/mip.hulian.top/components/mip-hulian-par/mip-hulian-par.vue
@@ -1,35 +1,31 @@
 <template>
-<div>
-<mip-iframe
-    :src="greeting"
-    allowfullscreen
-    width="100%"
-    :height="height"
-    allowtransparency="true">
-</mip-iframe>
-</div>
-
+  <div>
+    <mip-iframe
+      :src="greeting"
+      allowfullscreen
+      :height="height"
+      width="100%"
+      allowtransparency="true">
+    </mip-iframe>
+  </div>
 </template>
 
 <style scoped>
-    
-
 </style>
 
 <script>
-var hashstr = location.hash.replace('#','');
-var params = hashstr.split('&');
-var url = 'https://m.hulian.top/help/'+params[1].replace('type=','')+'/m/'+params[0].replace('id=','')+'.html'
-var height = MIP.viewport.getHeight('mip-iframe');
+let hashstr = location.hash.replace('#', '')
+let params = hashstr.split('&')
+let url = 'https://m.hulian.top/help/'+params[1].replace('type=', '')+'/m/'+params[0].replace('id=', '')+'.html'
+let height = MIP.viewport.getHeight('mip-iframe')
 export default {
-    // props: [],
-    computed: {
-      greeting() { 
-        return url;
-      },
-      height(){
-        return  height;
-      }
+  computed: {
+    greeting( ){ 
+        return url
+    },
+    height( ){
+      return height
     }
   }
+}
 </script>
