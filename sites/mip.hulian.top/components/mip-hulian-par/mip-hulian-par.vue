319a3bc315b6f98fe29edf85ba95aeca7ee76e4c
syjie <1211419561@qq.com>
2018-08-23 18:40:25 +0800

    Update mip-hulian-par.vue

--- sites/mip.hulian.top/components/mip-hulian-par/mip-hulian-par.vue
+++ sites/mip.hulian.top/components/mip-hulian-par/mip-hulian-par.vue
@@ -2,8 +2,8 @@
   <div>
     <mip-iframe
       :src="greeting"
-      allowfullscreen
       :height="height"
+      allowfullscreen
       width="100%"
       allowtransparency="true">
     </mip-iframe>
@@ -16,14 +16,14 @@
 <script>
 let hashstr = location.hash.replace('#', '')
 let params = hashstr.split('&')
-let url = 'https://m.hulian.top/help/'+params[1].replace('type=', '')+'/m/'+params[0].replace('id=', '')+'.html'
+let url = 'https://m.hulian.top/help/' + params[1].replace('type=', '') + '/m/' + params[0].replace('id=', '') + '.html'
 let height = MIP.viewport.getHeight('mip-iframe')
 export default {
   computed: {
-    greeting( ){ 
-        return url
+    greeting() {
+      return url
     },
-    height( ){
+    height() {
       return height
     }
   }
