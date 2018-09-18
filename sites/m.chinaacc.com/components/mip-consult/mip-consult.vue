1a0d7c3a8ce687106cff3b3ae9ed939f777d0685
Jordan_Q <yxq1002796635@163.com>
2018-09-18 11:39:05 +0800

    添加熊掌号js

--- sites/m.chinaacc.com/components/mip-consult/mip-consult.vue
+++ sites/m.chinaacc.com/components/mip-consult/mip-consult.vue
@@ -4,4 +4,60 @@
 </template>
 
 <script>
+import xzh from 'xzh-sdk'
+let context = null
+export default {
+  created () {
+    this.init()
+  },
+  methods: {
+    init () {
+      if (!context) {
+        let url = window.location.href
+        context = Promise.resolve()
+          .then(() => {
+            return window.fetchJsonp(
+              '//m.chinaacc.com/m_member/baidu/getXZJs.shtm?redirectUrl=' + url,
+              {
+                jsonpCallback: 'jsonpCallback'
+              }
+            )
+          })
+          .then(resp => {
+            return resp.json()
+          })
+          .then(data => {
+            return xzh.init({
+              data: {
+                appid: data.appid,
+                nonce_str: data.nonce_str,
+                signature: data.signature,
+                timestamp: data.timestamp,
+                url: data.url
+              }
+            })
+          })
+          .catch(err => {
+            console.log(err)
+          })
+      }
+      return context
+    },
+    async callIM () {
+      const that = this
+      try {
+        let { result: context } = await that.init()
+        // console.log(xzh);
+        await xzh.invokeBcpIM({
+          data: {
+            onlyWiseIM: true
+          },
+          context
+        })
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
+}
 </script>
--- sites/m.chinaacc.com/components/mip-submit-order/mip-submit-order.vue
+++ sites/m.chinaacc.com/components/mip-submit-order/mip-submit-order.vue
@@ -70,7 +70,7 @@
 
     <!--收件人信息-->
     <div
-      v-if="isShowAddress"
+      v-if="isShowAddress || hasBook > 0"
       class="sjrxx clearfix">
       <div class="tit clearfix">
         <div class="tit-left fl pr">
