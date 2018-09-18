a58d3b0f14d704f86b0cedfa8e90cb5d445daa67
motopia <haoyi731@gmail.com>
2018-09-18 18:09:36 +0800

    fix  一些组件问题
    Signed-off-by: motopia <haoyi731@gmail.com>

--- sites/moretickets.com/components/mip-list-type-nav/mip-list-type-nav.vue
+++ sites/moretickets.com/components/mip-list-type-nav/mip-list-type-nav.vue
@@ -38,106 +38,97 @@
 </template>
 
 <style scoped lang="less">
-.mip-fix-top{
-    top: 88px !important;
+.mip-fix-top {
+  top: 94px !important;
 }
 @main-color: #ff1d41;
-@boder-color:#eaeaeb;
-.box-flex(@flex){
-    -moz-box-flex: @flex;
-    -webkit-box-flex: @flex;
-    box-flex: @flex;
-    flex: @flex;
-    display: block;
+@boder-color: #eaeaeb;
+.list-type-nav-container {
+  border-bottom: 1px solid @boder-color;
+  background: #fff;
 }
-.list-type-nav-container{
-        display: flex;
-        align-items: center;
-        border-bottom: 1px solid @boder-color;
-        position: relative;
-        overflow: visible;
-        background: #fff;
-    }
-    .list-type-nav{
-        .box-flex(1);
-        overflow-x: auto;
-        overflow-y: hidden;
-        line-height: 5rem;
-        .type-nav-container{
-            min-width: 100%;
-            width: 58.5rem;
-            display: flex;
-            height: 5rem;
-            -webkit-overflow-scrolling: touch;
-            .type-nav{
-                padding: 0 1.3rem;
-                height: 100%;
-                text-align: center;
-                font-size: 1.4rem;
-                white-space: nowrap;
-                &.active{
-                    .type-name{
-                         border-bottom: 3px solid @main-color;
-                    }
-                    color: @main-color;
-                }
-            }
+.list-type-nav {
+  overflow-x: auto;
+  overflow-y: hidden;
+  line-height: 57px;
+  .type-nav-container {
+    min-width: 100%;
+    display: -webkit-box;
+    white-space: nowrap;
+    display: flex;
+    height: 60px;
+    -webkit-overflow-scrolling: touch;
+    .type-nav {
+      padding: 0 1.3rem;
+      height: 100%;
+      text-align: center;
+      font-size: 1.4rem;
+      white-space: nowrap;
+      display: inline-block;
+      flex-shrink: 0;
+      &.active {
+        .type-name {
+          border-bottom: 3px solid @main-color;
         }
+        color: @main-color;
+      }
     }
+  }
+}
 </style>
 
 <script>
 export default {
   props: {
-    'prefixUrl': {
+    prefixUrl: {
       type: String,
-      default: ''
+      default: ""
     },
-    'showTypeData': {
+    showTypeData: {
       type: Array,
       default: () => []
     },
-    'activeType': {
+    activeType: {
       type: String,
-      default: '1'
+      default: "1"
     },
-    'keyword': {
+    keyword: {
       type: String,
-      default: ''
+      default: ""
     }
   },
-  data () {
+  data() {
     return {
       showNav: true
-    }
+    };
   },
-  mounted () {
-    let me = this
-    console.log('列表导航加载ok!')
-    MIP.watch('keyword', newValue => {
-      me.showNav = newValue === ''
-      me.keyword = newValue
-    })
+  mounted() {
+    let me = this;
+    console.log("列表导航加载ok!");
+    MIP.watch("keyword", newValue => {
+      me.showNav = newValue === "";
+      me.keyword = newValue;
+    });
   },
   methods: {
-    resetKeyword () {
+    resetKeyword() {
       MIP.setData({
-        keyword: ''
-      })
+        keyword: ""
+      });
     },
-    switchType (type) {
+    switchType(type) {
       MIP.setData({
         activeType: type,
         loading: true
-      })
-      setTimeout(function () {
-        if (MIP.getData('loading')) {
+      });
+      setTimeout(function() {
+        if (MIP.getData("loading")) {
           MIP.setData({
             loading: false
-          })
+          });
         }
-      }, 2000)
+      }, 2000);
     }
   }
-}
+};
 </script>
