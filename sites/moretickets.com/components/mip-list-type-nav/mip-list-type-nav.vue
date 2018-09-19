fa1d724e4bfb71648e8e01dcce71296bd8ddcf85
motopia <haoyi731@gmail.com>
2018-09-19 11:52:43 +0800

    fix 搜索问题
    Signed-off-by: motopia <haoyi731@gmail.com>

--- sites/moretickets.com/components/mip-list-type-nav/mip-list-type-nav.vue
+++ sites/moretickets.com/components/mip-list-type-nav/mip-list-type-nav.vue
@@ -82,53 +82,83 @@ export default {
   props: {
     prefixUrl: {
       type: String,
-      default: ''
-    },
-    showTypeData: {
-      type: Array,
-      default: () => []
+      default: ""
     },
     activeType: {
       type: String,
-      default: '1'
+      default: "1"
     },
     keyword: {
       type: String,
-      default: ''
+      default: ""
     }
   },
-  data () {
+  data() {
     return {
-      showNav: true
-    }
+      showNav: true,
+      showTypeData: [
+        {
+          title: "演唱会",
+          type: "1"
+        },
+        {
+          title: "话剧歌剧",
+          type: "3"
+        },
+        {
+          title: "音乐会",
+          type: "2"
+        },
+        {
+          title: "儿童亲子",
+          type: "9"
+        },
+        {
+          title: "舞蹈芭蕾",
+          type: "5"
+        },
+        {
+          title: "展览休闲",
+          type: "7"
+        },
+        {
+          title: "曲艺杂谈",
+          type: "4"
+        },
+        {
+          title: "体育赛事",
+          type: "6"
+        }
+      ]
+    };
   },
-  mounted () {
-    let me = this
-    console.log('列表导航加载ok!')
-    MIP.watch('keyword', newValue => {
-      me.showNav = newValue === ''
-      me.keyword = newValue
-    })
+  mounted() {
+    let me = this;
+    console.log("列表导航加载ok!");
+    MIP.watch("keyword", newValue => {
+      me.showNav = newValue === "";
+      me.keyword = newValue;
+    });
   },
   methods: {
-    resetKeyword () {
+    resetKeyword() {
       MIP.setData({
-        keyword: ''
-      })
+        keyword: ""
+      });
     },
-    switchType (type) {
+    switchType(type) {
       MIP.setData({
         activeType: type,
         loading: true
-      })
-      setTimeout(function () {
-        if (MIP.getData('loading')) {
+      });
+      setTimeout(function() {
+        if (MIP.getData("loading")) {
           MIP.setData({
             loading: false
-          })
+          });
         }
-      }, 2000)
+      }, 2000);
     }
   }
-}
+};
 </script>
