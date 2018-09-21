a13d429d3cb117fff9b172ee63b767ccf037a4e0
JordansQ <yxq1002796635@163.com>
2018-09-21 16:58:04 +0800

    添加极速联调环境前缀地址 (#383)
    * 上传中华会计网网校代码
    
    * 先删除熊掌号js引用组件
    
    * 中华会计网校极速服务组件代码优化
    
    * 代码优化
    
    * 添加IM组件，代码优化
    
    * 代码优化
    
    * 添加xzh-sdk
    
    新增IM组件必须
    
    * 先删除xzh-sdk
    
    * 修改链接
    
    * 添加xzh-sdk
    
    添加xzh-sdk
    
    * 添加xzh-sdk
    
    添加xzh-sdk
    
    * 隐藏xzh-sdk
    
    xzh-sdk还不是白名单
    
    * 隐藏xzh-sdk
    
    xzh-sdk还不是白名单
    
    * 隐藏xzh-sdk
    
    xzh-sdk还未添加白名单
    
    * 隐藏xzh-sdk
    
    删除引用
    
    * 添加熊掌号js
    
    * 添加熊掌号js
    
    添加熊掌号js，优化代码
    
    * 修改xzh-sdk版本号
    
    * 添加极速联调环境前缀地址

--- sites/m.chinaacc.com/common/utils/config.js
+++ sites/m.chinaacc.com/common/utils/config.js
@@ -1,6 +1,6 @@
 export default {
   // 开发环境地址
-  devPageUrl: 'http://m.chinaacc.com/mip',
+  devPageUrl: 'https://m.baidu.com/wishwing/c/s/m.chinaacc.com/mip',
   // 通用部分请求地址
   commonUrl: '//sale.chinaacc.com/doorman/opjsonp?'
 }
