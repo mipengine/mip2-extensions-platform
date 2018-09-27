75516d2507eb39a51ec67b63e49bf2024556bd91
liu9183 <liuweijian01@baidu.com>
2018-09-27 13:34:57 +0800

    修改代码规范

--- sites/mobile.com/components/mip-mobile-download/mip-mobile-download.vue
+++ sites/mobile.com/components/mip-mobile-download/mip-mobile-download.vue
@@ -8,142 +8,142 @@
 </template>
 <script>
 export default {
-	props: {
-		url: {
-			type: String
-		},
-		btnStyle: {
-			type: Number,
-			default () {
-				return 1
-			}
-		},
-		source: {
-			type: String
-		},
-		versionCode: {
-			type: Number,
-			default () {
-				return 0
-			}
-		}
-	},
-	data() {
-		return {
-			dataList: {},
-			timer: null,
-			status: 'hidden',
-			//api: 'http://mobile.baidu.com/appsite?action=miphighdown&',
-			api: 'http://yq01-ae.epc.baidu.com:8777/appsite?action=miphighdown&',
-			styleObject: {
-				'radius': 'btn-radius',
-				'safe': 'mip-download-safe',
-				'disabled': 'mip-download-safe-invalid'
-			}
-		}
-	},
-	created() {
-		this.doLog('pageview', {
-			act: 'pageEnter',
-			from: this.source,
-			url: this.url
-		});
-		let url = this.getServerUrl();
-		let that = this;
-		fetch(url).then(function (res) {
-			return res.json();
-		}).then(function (data) {
-			if (data.error !== 0) {
-				that.status = 'hidden';
-				that.doLog('pageview', {
-					act: 'dataLoaded',
-					err: data.error,
-					tj: 'mip_download_' + that.url,
-					from: that.source
-				});
-			} else {
-				that.doLog('pageview', {
-					act: 'dataLoaded',
-					status: data.status,
-					tj: 'mip_download' + '_' + data.pid + '_' + data.docid,
-					from: that.source
-				});
-				that.status = data.status;
-			}
-			that.dataList = data;
-		}).catch(function (err) {
-			that.doLog('pageview', {
-				act: 'dataError',
-				req: url,
-				err: err
-			});
-			that.status = 'hidden';
-		})
-	},
-	methods: {
-		getServerUrl() {
-			let params = {
-				'url': this.url,
-				'from': this.source,
-				'versionCode': this.versionCode
-			};
-			let urlParams = this.obj2QueryStr(params);
-			return this.api + urlParams;
-		},
-		handleSafeClick() {
-			if (this.status === 'online') {
-				this.highdownByCoustomScheme(this.dataList.invokeParam, this.dataList.fallbackHighdown);
-			}
-		},
-		highdownByCoustomScheme(app, fallback) {
-			let timeout = 1000;
-			let start = Date.now();
-			let intent = 'bdashighdown://mobile.baidu.com/appsearch/highdownload?' + app;
-			let ifr = document.createElement('iframe');
-			let self = this;
-			ifr.width = 0;
-			ifr.height = 0;
-			ifr.style.opacity = 0;
-			ifr.src = intent;
-			document.body.appendChild(ifr);
-			if (this.timer) {
-				clearTimeout(this.timer);
-			}
-			this.timer = setTimeout(function () {
-				let unhidden = self.unHidden();
-				let timeDiff = Date.now() - start;
-				if (timeDiff < (timeout + 200) && unhidden) {
-					window.location.href = fallback;
-				}
-			}, timeout);
-		},
-		unHidden() {
-			if ('webkitHidden' in document) {
-				return !document.webkitHidden;
-			}
-			if ('mozHidden' in document) {
-				return !document.mozHidden;
-			}
-			if ('hidden' in document) {
-				return !document.hidden;
-			}
-			return true;
-		},
-		obj2QueryStr(params) {
-			return Object.keys(params).map(key => key + '=' + params[key]).join('&');
-		},
-		doLog(logtype, queryInfo = {}, headerInfo = {}) {
-			const LOG_BASE = 'https://mobile.baidu.com/app?';
-			queryInfo = Object.assign(queryInfo, {
-				action: 'log',
-				logtype: logtype,
-				mip: 'mip-download-safe'
-			});
-			let url = LOG_BASE + this.obj2QueryStr(queryInfo);
-			let img = document.createElement("img");
-			img.src = url;
-		}
-	}
+  props: {
+    url: {
+      type: String
+    },
+    btnStyle: {
+      type: Number,
+      default () {
+        return 1
+      }
+    },
+    source: {
+      type: String
+    },
+    versionCode: {
+      type: Number,
+      default () {
+        return 0
+      }
+    }
+  },
+  data() {
+    return {
+      dataList: {},
+      timer: null,
+      status: 'hidden',
+      //api: 'http://mobile.baidu.com/appsite?action=miphighdown&',
+      api: 'http://yq01-ae.epc.baidu.com:8777/appsite?action=miphighdown&',
+      styleObject: {
+        'radius': 'btn-radius',
+        'safe': 'mip-download-safe',
+        'disabled': 'mip-download-safe-invalid'
+      }
+    }
+  },
+  created() {
+    this.doLog('pageview', {
+      act: 'pageEnter',
+      from: this.source,
+      url: this.url
+    });
+    let url = this.getServerUrl();
+    let that = this;
+    fetch(url).then(function (res) {
+      return res.json();
+    }).then(function (data) {
+      if (data.error !== 0) {
+        that.status = 'hidden';
+        that.doLog('pageview', {
+          act: 'dataLoaded',
+          err: data.error,
+          tj: 'mip_download_' + that.url,
+          from: that.source
+        });
+      } else {
+        that.doLog('pageview', {
+          act: 'dataLoaded',
+          status: data.status,
+          tj: 'mip_download' + '_' + data.pid + '_' + data.docid,
+          from: that.source
+        });
+        that.status = data.status;
+      }
+      that.dataList = data;
+    }).catch(function (err) {
+      that.doLog('pageview', {
+        act: 'dataError',
+        req: url,
+        err: err
+      });
+      that.status = 'hidden';
+    })
+  },
+  methods: {
+    getServerUrl() {
+      let params = {
+        'url': this.url,
+        'from': this.source,
+        'versionCode': this.versionCode
+      };
+      let urlParams = this.obj2QueryStr(params);
+      return this.api + urlParams;
+    },
+    handleSafeClick() {
+      if (this.status === 'online') {
+        this.highdownByCoustomScheme(this.dataList.invokeParam, this.dataList.fallbackHighdown);
+      }
+    },
+    highdownByCoustomScheme(app, fallback) {
+      let timeout = 1000;
+      let start = Date.now();
+      let intent = 'bdashighdown://mobile.baidu.com/appsearch/highdownload?' + app;
+      let ifr = document.createElement('iframe');
+      let self = this;
+      ifr.width = 0;
+      ifr.height = 0;
+      ifr.style.opacity = 0;
+      ifr.src = intent;
+      document.body.appendChild(ifr);
+      if (this.timer) {
+        clearTimeout(this.timer);
+      }
+      this.timer = setTimeout(function () {
+        let unhidden = self.unHidden();
+        let timeDiff = Date.now() - start;
+        if (timeDiff < (timeout + 200) && unhidden) {
+          window.location.href = fallback;
+        }
+      }, timeout);
+    },
+    unHidden() {
+      if ('webkitHidden' in document) {
+        return !document.webkitHidden;
+      }
+      if ('mozHidden' in document) {
+        return !document.mozHidden;
+      }
+      if ('hidden' in document) {
+        return !document.hidden;
+      }
+      return true;
+    },
+    obj2QueryStr(params) {
+      return Object.keys(params).map(key => key + '=' + params[key]).join('&');
+    },
+    doLog(logtype, queryInfo = {}, headerInfo = {}) {
+      const LOG_BASE = 'https://mobile.baidu.com/app?';
+      queryInfo = Object.assign(queryInfo, {
+        action: 'log',
+        logtype: logtype,
+        mip: 'mip-download-safe'
+      });
+      let url = LOG_BASE + this.obj2QueryStr(queryInfo);
+      let img = document.createElement("img");
+      img.src = url;
+    }
+  }
 }
 </script>
 <style scoped>
