7c988f8b67d7c0b4c2e55cb09c9984d29cc751c8
jessli <zhidong.li@ilaw66.com>
2018-10-16 16:31:33 +0800

    0.1.1

---
+++ sites/ilaw66.com/components/mip-test1/README.md
@@ -0,0 +1,19 @@
+# `mip-test1`
+
+标题|内容
+----|----
+类型|
+支持布局|
+所需脚本| [https://c.mipcdn.com/extensions/platform/v2/ilaw66.com/mip-test1/mip-test1.js](https://c.mipcdn.com/extensions/platform/v2/ilaw66.com/mip-test1/mip-test1.js)
+
+## 说明
+
+组件功能说明
+
+## 示例
+
+示例说明
+
+## 属性
+
+属性说明
---
+++ sites/ilaw66.com/components/mip-test1/mip-test1.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="wrapper">
+    <h1>我是第一个组件</h1>
+    <h3>This is my first custom component !</h3>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  margin: 0 auto;
+  text-align: center;
+}
+</style>
+
+<script>
+export default {
+  mounted () {
+    console.log('This is my first custom component !')
+  }
+}
+</script>
