cd810c91ceb5a86dd118be96637d5ab4a4f7d585
FeliciaLFF <1400052652@qq.com>
2019-01-02 11:11:38 +0800

    testnewmodule

---
+++ sites/testnewmoudle7.no2.35nic.com/components/mip-testnewmoudle-carousel/mip-testnewmoudle-carousel.vue
@@ -0,0 +1,30 @@
+<template>
+    <mip-carousel autoplay defer="5000" layout="responsive" width="600" height="400">
+    <mip-img
+        src="https://www.mipengine.org/static/img/sample_01.jpg">
+    </mip-img>
+    <mip-img
+        src="https://www.mipengine.org/static/img/sample_02.jpg">
+    </mip-img>
+    <mip-img
+        src="https://www.mipengine.org/static/img/sample_03.jpg">
+    </mip-img>
+</mip-carousel>
+</template>
+
+<style scoped>
+.wrapper {
+  margin: 0 auto;
+  text-align: center;
+}
+</style>
+
+<script>
+
+export default {
+  mounted () {
+    console.log('This is my first custom component !')
+ 
+  }
+}
+</script>
