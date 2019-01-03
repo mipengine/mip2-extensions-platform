eb27179a91af51a33067bfe8a9d39ea7285880c8
FeliciaLFF <1450052652@qq.com>
2019-01-03 15:09:54 +0800

    修改mip-testnewmodule-shell为mip-tnm-shell，新增图片轮播mip-tnm-newscaro (#497)
    * .
    
    * .
    
    * .
    
    * .
    
    * .less
    
    * 修改mip-tnm-shell/mip-tnm-shell.less文件
    
    * 修改mip-tnm-shell/mip-tnm-shell.less文件缩进

---
+++ sites/testnewmoudle7.no2.35nic.com/components/mip-tnm-shell/README.md
@@ -0,0 +1,94 @@
+# mip-tnm-shell
+
+mip-tnm-shell 实现移动端站点头部，左侧菜单，底部菜单
+
+标题|mip-tnm-shell
+----|----
+类型|定制
+支持布局|responsive,fixed-height,fill,container,fixed
+所需脚本| [https://c.mipcdn.com/extensions/platform/v2/testnewmoudle7.no2.35nic.com/mip-tnm-shell/mip-tnm-shell.js](https://c.mipcdn.com/extensions/platform/v2/testnewmoudle7.no2.35nic.com/mip-tnm-shell/mip-tnm-shell.js)
+
+## 示例
+```html
+ <mip-tnm-shell mip-shell="">
+    <script type="application/json">
+    {
+      "routes": [
+        {
+          "pattern": "/use-shell.html",
+          "meta": {
+            "header": {
+              "show": true,
+              "title": "我的首页",
+              "logo": "https://gss0.bdstatic.com/5bd1bjqh_Q23odCf/static/wiseindex/img/favicon64.ico",
+               "nmlinkUrl":["/index.html","/single.html","/news.html","/products.html","/downs.html","/videos.html","/gbook.html","/jobs.html","/alinks.html","/search.html","/customform.html","javascript:void(0);","javascript:void(0);","/search.html","tel:","contactus.html","mailto:","sms:"]
+            },
+            "view": {
+              "isIndex": true
+            }
+          }
+        },
+        {
+          "pattern": "*",
+          "meta": {
+            "header": {
+              "show": true,
+              "title": "其他页面",
+              "nmlinkUrl":["/index.html","/single.html","/news.html","/products.html","/downs.html","/videos.html","/gbook.html","/jobs.html","/alinks.html","/search.html","/customform.html","javascript:void(0);","javascript:void(0);","/search.html","tel:","contactus.html","mailto:","sms:"]
+            }
+          }
+        }
+      ]
+    }
+            </script>
+        </mip-tnm-shell>
+
+示例说明
+
+## 属性  mip-shell
+
+属性说明  个性化shell
+
+Shell 配置的基本结构如下：
+   {
+      "routes": [
+        {
+          "pattern": "/use-shell.html",
+          "meta": {
+            "header": {
+              "show": true,
+              "title": "我的首页",
+              "logo": "",
+              "nmlinkUrl":[]
+            },
+            "view": {
+              "isIndex": true
+            }
+          }
+        },
+        {
+          "pattern": "*",
+          "meta": {
+            "header": {
+              "show": true,
+              "title": "其他页面",
+             "nmlinkUrl":[]
+            }
+          }
+        }
+      ]
+    }
+ 这里有几个注意点：
+
+       nmlinkUrl 根据类型判断链接页面
+       接口返回的数据格式需要是如下格式：
+       status：0 表示请求成功。
+       items：[] 是需要渲染的数序。
+       isEnd：表示是否是最后一页，非必须。
+       {
+            status: 0,
+            data: {
+                items: [],
+                isEnd: 1
+            }
+       }
---
+++ sites/testnewmoudle7.no2.35nic.com/components/mip-tnm-shell/mip-tnm-shell.js
@@ -0,0 +1,71 @@
+/**
+    @file 移动站点模板shell
+ * @author 1450052652@qq.com
+*/
+import './mip-tnm-shell.less'
+import Header from './feature/header'
+import Footer from './feature/footer'
+import Sidebar from './feature/sidebar'
+import apiUrl from '../../common/js/config.api'
+export default class MIPTestnewmoudleShell extends window.MIP.builtinComponents.MIPShell {
+  processShellConfig (shellConfig) {
+  }
+  renderOtherParts () {
+    // 底部菜单
+    this.$footerWrapper = document.createElement('mip-fixed')
+    this.$footerWrapper.setAttribute('type', 'bottom')
+    this.$footerWrapper.classList.add('mip-shell-footer-wrapper')
+
+    this.$footer = document.createElement('div')
+    this.$footer.classList.add('mip-shell-footer', 'mip-border')
+    this.$footer.innerHTML = this.renderFooter()
+
+    this.$footerWrapper.appendChild(this.$footer)
+    this.$sidebarWrapper = document.createElement('mip-sidebar') // 左侧菜单
+    this.$sidebarWrapper.setAttribute('id', 'sidebar')
+    this.$sidebarWrapper.className = 'left-sidebar'
+    this.$sidebarWrapper.setAttribute('display', 'nodisplay')
+    this.$sidebarWrapper.classList.add('mip-shell-sidebar-wrapper')
+    this.$sidebar = document.createElement('div')
+    this.$sidebar.classList.add('mip-shell-sidebar', 'mip-border')
+    this.$sidebar.innerHTML = this.renderSidebar()
+    this.$sidebarWrapper.appendChild(this.$sidebar)
+    super.renderOtherParts()
+    document.getElementsByTagName('mip-tnm-shell')[0].appendChild(this.$footerWrapper)
+    document.getElementsByTagName('mip-tnm-shell')[0].appendChild(this.$sidebarWrapper)
+    this._initAllObjects()
+  }
+  _initAllObjects () {
+    let configMeta = this.currentPageMeta
+    const isIndex = this.currentPageMeta.view.isIndex
+
+    this.header = new Header(this.$el);
+    (!isIndex) && this.header.setCustomHeader(apiUrl.homeUrl, apiUrl.nmsorttitleUrl)
+    this.footer = new Footer(this.$el)
+    this.footer.setCustomFooter(apiUrl.footermenuUrl, configMeta.header.nmlinkUrl)
+    this.sidebar = new Sidebar(this.$el)
+    this.sidebar.setCustomSidebar(apiUrl.sidebarmenuUrl,
+      configMeta.header.nmlinkUrl, apiUrl.lanUrl)
+  }
+  renderFooter () {
+    return ''
+  }
+  renderSidebar () {
+    return ''
+  }
+  updateOtherParts () {
+    this.$footer.innerHTML = this.renderFooter()
+    this.$sidebar.innerHTML = this.renderSidebar()
+    let configMeta = this.currentPageMeta
+    const isIndex = this.currentPageMeta.view.isIndex;
+    (!isIndex) && this.header.setCustomHeader(apiUrl.homeUrl, apiUrl.nmsorttitleUrl)
+    this.footer.setCustomFooter(apiUrl.footermenuUrl, configMeta.header.nmlinkUrl)
+    this.sidebar.setCustomSidebar(apiUrl.sidebarmenuUrl, configMeta.header.nmlinkUrl,
+      apiUrl.lanUrl)
+  }
+  handleShellCustomButton (buttonName) {
+  }
+  bindRootEvents () {
+    super.bindRootEvents()
+  }
+}
