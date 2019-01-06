fc72688f994655e47e6743d10f438452a2f86ed6
FeliciaLFF <1400052652@qq.com>
2019-01-06 11:21:30 +0800

    修改mip-tnm-newscaro轮播图接口

--- sites/testnewmoudle7.no2.35nic.com/common/js/config.api.js
+++ sites/testnewmoudle7.no2.35nic.com/common/js/config.api.js
@@ -5,7 +5,7 @@
  */
 const baseurl = 'https://testnewmoudle7.no2.35nic.com/'
 const apiUrl = {
-  bannerList: baseurl + 'open_webapi/MofineApi.asmx/GetTempNewsList?menuIndexVar=travel_news&editionId=1&intSta=0&intNum=2', //   首页轮播图接口
+  bannerList: baseurl + 'open_webapi/MofineApi.asmx/GetTempNewsList?menuIndexVar=travel_news&editionId=1&intSta=0&intNum=2&sortId=0', //   首页轮播图接口
   homeUrl: baseurl + 'mip/templates/default/index.html', // 首页链接
   footermenuUrl: baseurl + 'open_webapi/MofineApi.asmx/GetBottomMenu?editionId=1&intTop=5', // 底部菜单接口
   sidebarmenuUrl: baseurl + 'open_webapi/MofineApi.asmx/GetQuickMenu?editionId=1&intTop=10', // 左侧菜单接口
