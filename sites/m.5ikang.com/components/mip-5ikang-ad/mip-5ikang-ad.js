2e84aa65cf8fd510a1edbe237c618eb30f7ad22d
ikangqd <5ikangrd@gmail.com>
2019-01-23 13:34:16 +0800

    eslint edit

--- sites/m.5ikang.com/components/mip-5ikang-ad/mip-5ikang-ad.js
+++ sites/m.5ikang.com/components/mip-5ikang-ad/mip-5ikang-ad.js
@@ -1,23 +1,21 @@
 export default class MIP5ikangAd extends MIP.CustomElement {
-
   build() {
     let loadIkAd = function (elem) {
       let config = elem.getAttribute('config')
-      let div = document.createElement('div');
+      let div = document.createElement('div')
       div.setAttribute('class', `${config}_div`)
       elem.appendChild(div)
 
-      let adConfig = MIP.getData('adConfig');
+      let adConfig = MIP.getData('adConfig')
       if (!adConfig) {
         adConfig = ''
       }
       adConfig += config + '|'
       MIP.setData({
         adConfig: adConfig
-      });
-    };
-
-    let element = this.element;
+      })
+    }
+    let element = this.element
     loadIkAd(element)
   }
 }
