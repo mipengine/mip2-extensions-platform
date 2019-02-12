f7bbe4da1cbaf8037f4eac8ac159a6b6e1fffa47
volcano <east.qrj@gmail.com>
2019-02-12 14:22:30 +0800

    add woyaogexing.com

---
+++ sites/woyaogexing.com/components/mip-app-download/README.md
@@ -0,0 +1,11 @@
+# mip-app-download
+
+标题|内容
+----|----
+类型|
+支持布局|
+所需脚本| [https://c.mipcdn.com/extensions/platform/v2/woyaogexing.com/mip-app-download/mip-app-download.js](https://c.mipcdn.com/extensions/platform/v2/woyaogexing.com/mip-app-download/mip-app-download.js)
+
+## 说明
+
+我要个性页面APP下载
---
+++ sites/woyaogexing.com/components/mip-app-download/mip-app-download.vue
@@ -0,0 +1,32 @@
+<template>
+  <div @click="downloadApp">
+    <mip-img :src="url" />
+  </div>
+</template>
+<script>
+let platform = MIP.util.platform
+export default {
+  data: () => {
+    return {
+      url: ''
+    }
+  },
+  created () {
+    let url = this.$element.getAttribute('src')
+    this.url = url
+  },
+  methods: {
+    downloadApp () {
+      let element = this.$element
+      let downloadUrl
+      if (platform.isIos()) {
+        downloadUrl = element.getAttribute('data-ios') ? element.getAttribute('data-ios') : ''
+      }
+      if (platform.isAndroid()) {
+        downloadUrl = element.getAttribute('data-android') ? element.getAttribute('data-android') : ''
+      }
+      window.location.href = downloadUrl
+    }
+  }
+}
+</script>
