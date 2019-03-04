57fd5c3c2addea7dd9d6d2ea343554c169b46280
regedit <regedit.email@gmail.com>
2019-03-04 10:35:39 +0800

    新增mip-ys137-box组件 (#528)
    * 新增mip-ys137-box组件
    
    * 修改格式
    
    * 修改样例
    
    * 修改
    
    * 修改格式错误
    
    * 修改组件执行事件init为标准的connectedCallback

---
+++ sites/ys137.com/components/mip-ys137-box/mip-ys137-box.js
@@ -0,0 +1,24 @@
+import './index.less'
+
+export default class MIPYs137Box extends MIP.CustomElement {
+  connectedCallback () {
+    this.myinit()
+  }
+  myinit () {
+    const element = this.element
+    const action = element.getAttribute('action')
+    switch (action) {
+      case 'show':
+        break
+      case 'hide':
+        element.style.display = 'none'
+        break
+      case 'remove':
+        element.remove()
+        break
+      default:
+        element.remove()
+        break
+    }
+  }
+}
