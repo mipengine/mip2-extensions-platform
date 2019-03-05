4a01aea4e1081875a67b6aa2497ccf7d010341a8
lxy <lixiangyang@51credit.com>
2019-03-05 13:30:39 +0800

    修改跳转方法

--- sites/m.51credit.com/components/mip-51credit-getck/mip-51credit-getck.js
+++ sites/m.51credit.com/components/mip-51credit-getck/mip-51credit-getck.js
@@ -11,7 +11,8 @@ export default class MIP51creditGetck extends MIP.CustomElement {
       hrUrl += '&bd_c=' + encodeURIComponent(getCookie('Hm_lvt_e06f837c19a278d81816c90560e0d82d'))
     }
     this.element.addEventListener('click', function () {
-      location.href = hrUrl
+      // MIP页会在百度搜索结果页以iframe内嵌形式打开，跳转建议使用 window.top.location.href
+      window.top.location.href = hrUrl
     })
   }
 }
