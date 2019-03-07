801070773b8a18ed02baa5a456cd169ee2435d3a
regedit <regedit.email@gmail.com>
2019-03-07 16:45:56 +0800

    增加判断条件关键词：!（非）和|(或） (#542)
    * 新增mip-ys137-box组件
    
    * 修改格式
    
    * 修改样例
    
    * 修改
    
    * 修改格式错误
    
    * 修改组件执行事件init为标准的connectedCallback
    
    * 新增自定义百度统计组件
    
    * 格式化代码
    
    * 删除mip-ys137-box组件
    
    * 增加判断条件关键词：!（非）和|(或）
    
    * 修改格式
    
    * 删除调试信息
    
    * 修复冲突代码
    
    * 修改冲突
    
    * 删除备份文件
    
    * 格式化代码

--- sites/ys137.com/components/mip-ys137-baidustats/mip-ys137-baidustats.js
+++ sites/ys137.com/components/mip-ys137-baidustats/mip-ys137-baidustats.js
@@ -4,12 +4,31 @@ export default class MIPYs137Box extends MIP.CustomElement {
   connectedCallback () {
     let referrer = MIP.sandbox.window.document.referrer
     let referrerCheck = this.element.getAttribute('referrer')
-    if (referrerCheck) {
-      if (referrer.indexOf(referrerCheck) !== -1) {
-        this.createBaidustats()
-      }
-    } else {
+    if (this.refIsexisted(referrer, referrerCheck) === true) {
       this.createBaidustats()
+    }
+  }
+  refIsexisted (referrer, referrerCheck) {
+    if (referrerCheck) {
+      let checkArr = []
+      let trueValue = true
+      if (referrerCheck.substr(0, 1) === '!') {
+        trueValue = false
+        referrerCheck = referrerCheck.substring(1)
+      }
+      if (referrerCheck.indexOf('|') !== -1) {
+        checkArr = referrerCheck.split('|')
+      } else {
+        checkArr.push(referrerCheck)
+      }
+      for (let i = 0; i < checkArr.length; i++) {
+        if (referrer.indexOf(checkArr[i]) !== -1) {
+          return trueValue
+        }
+      }
+      return !trueValue
+    } else {
+      return true
     }
   }
   createBaidustats () {
