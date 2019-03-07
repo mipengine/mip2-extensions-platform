307e43d83fb20431a5c0f2abb01a48862200ef53
unknown <regedit.email@gmail.com>
2019-03-07 13:03:00 +0800

    增加判断条件关键词：!（非）和|(或）

--- sites/ys137.com/components/mip-ys137-baidustats/mip-ys137-baidustats.js
+++ sites/ys137.com/components/mip-ys137-baidustats/mip-ys137-baidustats.js
@@ -4,12 +4,35 @@ export default class MIPYs137Box extends MIP.CustomElement {
   connectedCallback () {
     let referrer = MIP.sandbox.window.document.referrer
     let referrerCheck = this.element.getAttribute('referrer')
-    if (referrerCheck) {
-      if (referrer.indexOf(referrerCheck) !== -1) {
-        this.createBaidustats()
-      }
-    } else {
+    if (this.refIsexisted(referrer, referrerCheck) === true) {
+      console.log('true')
       this.createBaidustats()
+    } else {
+      console.log('false')
+    }
+  }
+  refIsexisted (referrer, referrerCheck) {
+    if (referrerCheck) {
+      let checkArr = new Array()
+      let trueValue = true
+      if (referrerCheck.substr(0,1) === '!') {
+        trueValue = false
+        referrerCheck=referrerCheck.substring(1)
+      }
+      if (referrerCheck.indexOf('|') !== -1) {
+        checkArr = referrerCheck.split('|')
+      } else {
+        checkArr.push(referrerCheck)
+      }
+      
+      for (let i=0; i<checkArr.length; i++) {
+        if (referrer.indexOf(checkArr[i]) !== -1) {
+          return trueValue
+        }
+      }
+      return !trueValue
+    } else {
+      return true
     }
   }
   createBaidustats () {
