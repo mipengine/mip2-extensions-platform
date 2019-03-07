c2b29a67601ca3065dd551f3e16ae21f160c36a2
unknown <regedit.email@gmail.com>
2019-03-07 13:27:17 +0800

    修复冲突代码

--- sites/ys137.com/components/mip-ys137-baidustats/mip-ys137-baidustats.js
+++ sites/ys137.com/components/mip-ys137-baidustats/mip-ys137-baidustats.js
@@ -5,10 +5,6 @@ export default class MIPYs137Box extends MIP.CustomElement {
     let referrer = MIP.sandbox.window.document.referrer
     let referrerCheck = this.element.getAttribute('referrer')
     if (this.refIsexisted(referrer, referrerCheck) === true) {
-      this.createBaidustats()
-    }
-  }
-  refIsexisted (referrer, referrerCheck) {
     if (referrerCheck) {
       let checkArr = new Array()
       let trueValue = true
