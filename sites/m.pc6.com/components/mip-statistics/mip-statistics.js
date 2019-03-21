8fdb638019c12faa4b41deee6824c0032bfccd6e
fangle1212 <592825310@qq.com>
2019-03-21 13:35:01 +0800

    statistics自定义统计

--- sites/m.pc6.com/components/mip-statistics/mip-statistics.js
+++ sites/m.pc6.com/components/mip-statistics/mip-statistics.js
@@ -1,26 +1,28 @@
 import './index.less'
 const { CustomElement, util } = MIP
 const { jsonParse, dom } = util
-export default class MIPStatistics extends MIP.CustomElement {
-  data = {
-    dateTime: document.getElementById('down-href').getAttribute('dateTime'),
-    username: document.getElementById('down-href').getAttribute('username')
+export default class MIPStatistics extends CustomElement {
+  data () {
+    return {
+      dateTime: document.getElementById('down-href').getAttribute('dateTime'),
+      username: document.getElementById('down-href').getAttribute('username')
+    }
   }
-  build() {
+  build () {
     let n = document.querySelector('script[type="application/json"]')
     if (n) {
-      let a = jsonParse(n.textContent.toString().replace(/[\s\b\t]/g, ""))
-      if (void 0 !== this.data.dateTime && "" !== this.data.username && a) {
-        var r = "";
+      let a = jsonParse(n.textContent.toString().replace(/[\s\b\t]/g, ''))
+      if (void 0 !== this.data.dateTime && '' !== this.data.username && a) {
+        let r = ''
         if (a[0][this.data.username]) {
           if (a[0][this.data.username].xtime) {
-            var o = new Date(a[0][this.data.username].xtime),
-              s = new Date(this.data.dateTime);
+            let o = new Date(a[0][this.data.username].xtime)
+            let s = new Date(this.data.dateTime)
             if (o.getTime() < s.getTime()) {
               r = a[0][this.data.username].hmToken
             }
           } else { r = a[0][this.data.username].hmToken }
-          if ("" !== r) {
+          if ('' !== r) {
             let stat = dom.create(`<mip-stats-baidu token="${r}"></mip-stats-baidu>`)
             this.element.appendChild(stat)
           }
