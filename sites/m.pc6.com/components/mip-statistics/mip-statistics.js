4743a374a7ece49030aae57d5350df42d0d0b20a
fangle1212 <592825310@qq.com>
2019-03-21 13:49:10 +0800

    statistics alter

--- sites/m.pc6.com/components/mip-statistics/mip-statistics.js
+++ sites/m.pc6.com/components/mip-statistics/mip-statistics.js
@@ -12,7 +12,7 @@ export default class MIPStatistics extends CustomElement {
     let n = document.querySelector('script[type="application/json"]')
     if (n) {
       let a = jsonParse(n.textContent.toString().replace(/[\s\b\t]/g, ''))
-      if (void 0 !== this.data().dateTime && '' !== this.data().username && a) {
+      if (void 0 !== this.data().dateTime && '' != this.data().username && a) {
         let r = ''
         if (a[0][this.data().username]) {
           if (a[0][this.data().username].xtime) {
@@ -22,7 +22,7 @@ export default class MIPStatistics extends CustomElement {
               r = a[0][this.data().username].hmToken
             }
           } else { r = a[0][this.data().username].hmToken }
-          if ('' !== r) {
+          if ('' != r) {
             let stat = dom.create(`<mip-stats-baidu token="${r}"></mip-stats-baidu>`)
             this.element.appendChild(stat)
           }
