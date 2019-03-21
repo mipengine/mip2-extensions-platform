c4662a4cf42b381471d584e6acdbf00db66710bb
fangle1212 <592825310@qq.com>
2019-03-21 13:39:03 +0800

    statistics 函数修改

--- sites/m.pc6.com/components/mip-statistics/mip-statistics.js
+++ sites/m.pc6.com/components/mip-statistics/mip-statistics.js
@@ -12,16 +12,16 @@ export default class MIPStatistics extends CustomElement {
     let n = document.querySelector('script[type="application/json"]')
     if (n) {
       let a = jsonParse(n.textContent.toString().replace(/[\s\b\t]/g, ''))
-      if (void 0 !== this.data.dateTime && '' !== this.data.username && a) {
+      if (void 0 !== this.data().dateTime && '' !== this.data().username && a) {
         let r = ''
-        if (a[0][this.data.username]) {
-          if (a[0][this.data.username].xtime) {
-            let o = new Date(a[0][this.data.username].xtime)
-            let s = new Date(this.data.dateTime)
+        if (a[0][this.data().username]) {
+          if (a[0][this.data().username].xtime) {
+            let o = new Date(a[0][this.data().username].xtime)
+            let s = new Date(this.data().dateTime)
             if (o.getTime() < s.getTime()) {
-              r = a[0][this.data.username].hmToken
+              r = a[0][this.data().username].hmToken
             }
-          } else { r = a[0][this.data.username].hmToken }
+          } else { r = a[0][this.data().username].hmToken }
           if ('' !== r) {
             let stat = dom.create(`<mip-stats-baidu token="${r}"></mip-stats-baidu>`)
             this.element.appendChild(stat)
