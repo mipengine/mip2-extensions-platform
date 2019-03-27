f13b41c1e708b4e40a287f397cb2d4569f72fd8f
lj <jishulijie@dingtalk.com>
2019-03-27 16:27:57 +0800

    修改
    修改平台判断和属性

--- sites/idongdong.com/components/mip-idongdong-address/mip-idongdong-address.js
+++ sites/idongdong.com/components/mip-idongdong-address/mip-idongdong-address.js
@@ -2,26 +2,25 @@ import './index.less'
 
 export default class MIPIdongdongAddress extends MIP.CustomElement {
   build () {
+    let platform = MIP.util.platform
     let u = navigator.userAgent
-    let isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1
-    let isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/)
     let obj = this.element.querySelector('#btns a')
-    let isDown = obj.getAttribute('isDown')
+    let isDown = obj.getAttribute('isdown')
     let xgIphone = this.element.querySelector('#plat_iPhone') !== null
     let xgAndroid = this.element.querySelector('#plat_Android') !== null
-    if (isiOS && !xgIphone && isDown === '0') {
+    if (platform.isIos() && !xgIphone && isDown === '0') {
       obj.classList.add('noDown')
       obj.setAttribute('href', 'javascript:void(0)')
       obj.innerHTML = `
         暂无苹果版
       `
-    } else if (isAndroid && !xgAndroid && isDown === '0') {
+    } else if (platform.isAndroid() && !xgAndroid && isDown === '0') {
       obj.classList.add('noDown')
       obj.setAttribute('href', 'javascript:void(0)')
       obj.innerHTML = `
         暂无安卓版
       `
-    } else if (!isAndroid && !isiOS && isDown === '0') {
+    } else if (!platform.isAndroid() && !platform.isIos() && isDown === '0') {
       obj.classList.add('noDown')
       obj.setAttribute('href', 'javascript:void(0)')
       obj.innerHTML = `
