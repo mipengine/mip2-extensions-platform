811134fcbe2530974481b6450a4d8039a1f11bb6
unknown <regedit.email@gmail.com>
2019-04-02 15:16:38 +0800

    去掉documentElement

--- sites/ys137.com/components/mip-ys137-box/mip-ys137-box.js
+++ sites/ys137.com/components/mip-ys137-box/mip-ys137-box.js
@@ -197,7 +197,7 @@ export default class MIPYs137Box extends MIP.CustomElement {
     const scope = element.getAttribute('scope')
     const isOk = scopeOk(scope) // 检测scope是否合规
     const targetId = element.getAttribute('targetId')
-    const targetDom = targetId !== '' ? document.documentElement.querySelector('#' + targetId) : null
+    const targetDom = targetId !== '' ? querySelector('#' + targetId) : null
     if (!isOk) {
       // 检测不合规时将内容清空
       if (targetDom !== null) {
