6c3a33ca535c2c35bb5687091fb144fd6fa51a58
muliqun <muliqun@qudao.com>
2019-04-12 16:51:48 +0800

    起点餐饮搜索组件

---
+++ sites/m.cyqidian.com/components/mip-search-layer/README.md
@@ -0,0 +1,26 @@
+# mip-search-layer
+
+mip-search-layer 组件说明
+
+标题|内容
+----|----
+类型|通用
+支持布局|responsive,fixed-height,fill,container,fixed
+所需脚本| [https://c.mipcdn.com/static/v2/mip-form/mip-form.js,http://mipcache.bdstatic.com/static/mip-search-layer/{版本号}/mip-search-layer.js]
+
+## 说明
+
+网站中搜索弹出层组件
+
+## 示例
+
+### 基本用法
+
+```html
+<mip-search-layer></mip-search-layer>
+```
+
+## 属性
+
+## 注意事项
+
---
+++ sites/m.cyqidian.com/components/mip-search-layer/mip-search-layer.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <header class="cymip-header sidebar-header">
+      <span class="cymip-hd-goback" @click="$emit('close')"></span>
+      <div class="cymip-search">
+        <mip-form url="/" method="get" class="search-form">
+          <input type="text" class="search-input" name="key" value placeholder="输入您想要找的项目">
+          <input type="button" class="search-button" name value="搜索">
+        </mip-form>
+      </div>
+    </header>
+    <section class="sidebar-body">
+      <div class="hot-keyword">
+        <h3 class="hotkey-title">热门搜索</h3>
+        <div class="hotkey-list">
+          <a data-type="mip" v-for="(item,i) in items" :href="item.action" class="" :title="item.name" :key="i">{{item.name}}</a>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<style lang="less" scoped>
+</style>
+
+<script>
+export default {
+  props: {
+    items:{
+      type:Array,
+      default:()=>{
+        return [];
+      }
+    }
+  },
+  data() {
+    return {
+      show:false
+    };
+  },
+  mounted() {
+    console.log(this.items);
+  },
+  methods: {
+    open() {
+    }
+  }
+};
+</script>
