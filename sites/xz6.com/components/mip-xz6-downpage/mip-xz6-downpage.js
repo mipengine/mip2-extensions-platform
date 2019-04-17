6f8bc6baa4356d935308ec87dc008afa48d59a73
lj <jishulijie@dingtalk.com>
2019-04-17 14:50:01 +0800

    规范代码

--- sites/xz6.com/components/mip-xz6-downpage/mip-xz6-downpage.js
+++ sites/xz6.com/components/mip-xz6-downpage/mip-xz6-downpage.js
@@ -1,24 +1,24 @@
 export default class MIPXz6Down extends MIP.CustomElement {
   build () {
-    let tzz  = this.element.querySelector('.tzz')
+    let tzz = this.element.querySelector('.tzz')
     let bookCata = this.element.querySelector('#bookCata')
     let readAdd = this.element.querySelector('.freeBook').getAttribute('data-read')
     let btns = this.element.querySelector('.btns')
     let goRead = btns.querySelector('.goRead')
     let moerChapter = this.element.querySelector('#moer-chapter')
     if (tzz.querySelectorAll('li').length === 0) {
-        this.element.querySelector('#mip-toggle').removeChild(tzz)
+      this.element.querySelector('#mip-toggle').removeChild(tzz)
     }
     if (bookCata.querySelectorAll('#chapter-list li').length < 1) {
-        if (bookCata.querySelectorAll('#chapter-list li').length === 0 && readAdd !== '') {
-            goRead.setAttribute('href', readAdd)
-        } else {
-            btns.removeChild(goRead)
-        }
-        this.element.querySelector('#mip-toggle').removeChild(bookCata)
+      if (bookCata.querySelectorAll('#chapter-list li').length === 0 && readAdd !== '') {
+        goRead.setAttribute('href', readAdd)
+      } else {
+        btns.removeChild(goRead)
+      }
+      this.element.querySelector('#mip-toggle').removeChild(bookCata)
     }
     else if (bookCata.querySelectorAll('#chapter-list li').length === 1) {
-        moerChapter.style.display = 'none'
+      moerChapter.style.display = 'none'
     }
   }
 }
