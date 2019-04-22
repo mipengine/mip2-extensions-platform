47adfcf32454d9be0608fb5ac8ca640c72d1375a
leizhen <leihzen@gmail.com>
2019-04-22 14:22:50 +0800

    over

--- sites/m.xuannaer.com/components/mip-xuannaer-exchange/mip-xuannaer-exchange.js
+++ sites/m.xuannaer.com/components/mip-xuannaer-exchange/mip-xuannaer-exchange.js
@@ -2,8 +2,11 @@ import './index.less'
 const {
   util
 } = MIP
+const {
+  fetchJsonp
+} = window
 export default class MIPXuannaerExchange extends MIP.CustomElement {
-  build() {
+  build () {
     this.exchange = document.querySelector('.intro-but')
     this.showThat = document.querySelector('.m-base-info-detail')
     this.bg = document.querySelectorAll('.m-product-base-info')
@@ -12,9 +15,7 @@ export default class MIPXuannaerExchange extends MIP.CustomElement {
     this.floorPrice = document.querySelector('.completion_floor_price')
     this.landPrice = document.querySelector('.land_price')
     this.premiumRate = document.querySelector('.premium_rate')
-    this.needHide = document.querySelectorAll('exchange-hide')
-    this.needShow = document.querySelectorAll('exchange-show')
-    //
+    //处理数据的显示和单位的转化
     this.dataArr = this.getData()
     this.nowStatus()
     this.showThat.addEventListener('click', () => {
@@ -24,14 +25,14 @@ export default class MIPXuannaerExchange extends MIP.CustomElement {
       this.exchangeStatus()
     }, false)
   }
-  nowStatus() {
+  nowStatus () {
     fetchJsonp(`${this.dataArr['hostUrl']}Mipplugin/recruitPrice/id/${this.dataArr['detailId']}`).then(res => {
       return res.json()
     }).then(result => {
       if (result) {
         this.bg.forEach(function (evt) {
           evt.classList.remove('m-background')
-        });
+        })
         util.css(this.showThat, 'display', 'none')
         if (result[0]) {
           this.completionPrice.innerHTML = `${result[0]}万元`
@@ -39,7 +40,7 @@ export default class MIPXuannaerExchange extends MIP.CustomElement {
           this.completionPrice.innerHTML = '暂无数据'
         }
         if (result[1]) {
-          this.floorPrice.innerHTML = `<span class="exchange-show">${result[1]}元/㎡</span><span class="exchange-hide"> (${(result[1] * 666.667 / 10000).toFixed(2)}万元/亩</span>`
+          this.floorPrice.innerHTML = `<span class="exchange-show">${result[1]}元/㎡</span><span class="exchange-hide">${(result[1] * 666.667 / 10000).toFixed(2)}万元/亩</span>`
         } else {
           this.floorPrice.innerHTML = '暂无数据'
         }
@@ -56,20 +57,20 @@ export default class MIPXuannaerExchange extends MIP.CustomElement {
       }
     })
   }
-  getStatus() {
+  getStatus () {
     fetchJsonp(`${this.dataArr['hostUrl']}Mipplugin/recruitClickPrice/id/${this.dataArr['detailId']}`).then(res => {
       return res.json()
     }).then(result => {
       if (result && result['code'] == 1) {
-        window.location.href = `${this.dataArr['imsUrl']}/user/login/login_tab`;
+        window.location.href = `${this.dataArr['imsUrl']}/user/login/login_tab`
       }
       if (result && result['code'] == 500) {
-        this.tipModal(result['msg']);
+        this.tipModal(result['msg'])
       }
       if (result && result['code'] == 200) {
         this.bg.forEach(function (evt) {
           evt.classList.remove('m-background')
-        });
+        })
         util.css(this.showThat, 'display', 'none')
         if (result[0]) {
           this.completionPrice.innerHTML = `${result[0]}万元`
@@ -94,7 +95,7 @@ export default class MIPXuannaerExchange extends MIP.CustomElement {
       }
     })
   }
-  getData() {
+  getData () {
     let params = {
       "imsUrl": "",
       "imgUrl": "",
@@ -107,23 +108,25 @@ export default class MIPXuannaerExchange extends MIP.CustomElement {
     }
     return params
   }
-  exchangeStatus() {
+  exchangeStatus () {
+    this.needHide = document.querySelectorAll('.exchange-hide')
+    this.needShow = document.querySelectorAll('.exchange-show')
     if (this.exchange.classList.contains('exchagearea')) {
       this.needShow.forEach(function (evt) {
-        util(evt, 'display', 'block')
-      });
+        util.css(evt, 'display', 'block')
+      })
       this.needHide.forEach(function (evt) {
-        util(evt, 'display', 'none')
-      });
+        util.css(evt, 'display', 'none')
+      })
       this.exchange.innerHTML = '切换面积为(亩)'
       this.exchange.classList.remove('exchagearea')
     } else {
       this.needShow.forEach(function (evt) {
-        util(evt, 'display', 'none')
-      });
+        util.css(evt, 'display', 'none')
+      })
       this.needHide.forEach(function (evt) {
-        util(evt, 'display', 'block')
-      });
+        util.css(evt, 'display', 'block')
+      })
       this.exchange.innerHTML = '切换面积为(㎡)'
       this.exchange.classList.add('exchagearea')
     }
