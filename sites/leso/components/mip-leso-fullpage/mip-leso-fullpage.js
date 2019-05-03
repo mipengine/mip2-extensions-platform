763e618a64d48e43c44c74e26979518967c85bcb
mip-platform <mip-platform@baidu.com>
2019-05-03 21:13:48 +0800

    mip-platform auto create Pull Request

--- sites/leso/components/mip-leso-fullpage/mip-leso-fullpage.js
+++ sites/leso/components/mip-leso-fullpage/mip-leso-fullpage.js
@@ -1,15 +1,15 @@
 import './index.less'
 
 export default class MIPLesoFullpage extends MIP.CustomElement {
-  build () {
+  build() {
     // 设定尺寸
-    let fullbody=MIP.viewport.getHeight()
-    MIP.util.css(this.element,'height',fullbody)
+    let fullbody = MIP.viewport.getHeight()
+    MIP.util.css(this.element, 'height', fullbody)
     let section = document.getElementsByClassName('fullpage')
-    MIP.util.css(section,'height',fullbody)
+    MIP.util.css(section, 'height', fullbody)
     let alp = document.querySelector('#allpage')
-    MIP.util.css(alp,'top','0px')
-    
+    MIP.util.css(alp, 'top', '0px')
+
     // 添加滚动条件
     let Gesture = MIP.util.Gesture
     let gesture = new Gesture(this.element)
@@ -17,15 +17,15 @@ export default class MIPLesoFullpage extends MIP.CustomElement {
     let allsec = alp.attributes.sec.value
 
     gesture.on('swipeup swipedown', function (event, data) {
-      if(data.swipeDirection=="up"){
-        if(nowsec<allsec){
-          MIP.util.css(alp,'top','-'+fullbody*nowsec+'px')
+      if (data.swipeDirection == "up") {
+        if (nowsec < allsec) {
+          MIP.util.css(alp, 'top', '-' + fullbody * nowsec + 'px')
           nowsec++;
         }
       }
-      if(data.swipeDirection=="down"){
-        if(nowsec>1){
-          MIP.util.css(alp,'top','-'+fullbody*(nowsec-2)+'px')
+      if (data.swipeDirection == "down") {
+        if (nowsec > 1) {
+          MIP.util.css(alp, 'top', '-' + fullbody * (nowsec - 2) + 'px')
           nowsec--;
         }
       }
