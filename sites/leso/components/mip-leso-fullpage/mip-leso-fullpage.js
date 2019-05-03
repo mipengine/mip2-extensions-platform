390ff8eff263a698834676b055c813c38eae06c2
mip-platform <mip-platform@baidu.com>
2019-05-03 20:47:15 +0800

    mip-platform auto create Pull Request

---
+++ sites/leso/components/mip-leso-fullpage/mip-leso-fullpage.js
@@ -0,0 +1,34 @@
+import './index.less'
+
+export default class MIPLesoFullpage extends MIP.CustomElement {
+  build () {
+    // 设定尺寸
+    let fullbody=MIP.viewport.getHeight()
+    MIP.util.css(this.element,'height',fullbody)
+    let section = document.getElementsByClassName('fullpage')
+    MIP.util.css(section,'height',fullbody)
+    let alp = document.querySelector('#allpage')
+    MIP.util.css(alp,'top','0px')
+    
+    // 添加滚动条件
+    let Gesture = MIP.util.Gesture
+    let gesture = new Gesture(this.element)
+    let nowsec = 1;
+    let allsec = alp.attributes.sec.value
+
+    gesture.on('swipeup swipedown', function (event, data) {
+      if(data.swipeDirection=="up"){
+        if(nowsec<allsec){
+          MIP.util.css(alp,'top','-'+fullbody*nowsec+'px')
+          nowsec++;
+        }
+      }
+      if(data.swipeDirection=="down"){
+        if(nowsec>1){
+          MIP.util.css(alp,'top','-'+fullbody*(nowsec-2)+'px')
+          nowsec--;
+        }
+      }
+    });
+  }
+}
