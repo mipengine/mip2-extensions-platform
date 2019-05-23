6dae4f9dcdc5f894b0ded5bd96795e05b822bf29
mip-platform <mip-platform@baidu.com>
2019-05-23 15:02:47 +0800

    mip-platform auto create Pull Request

--- sites/china.cn/components/mip-chinacn-stats/mip-chinacn-stats.js
+++ sites/china.cn/components/mip-chinacn-stats/mip-chinacn-stats.js
@@ -1,13 +1,13 @@
 import config from './config.js'
 
 export default class MIPChinacnStats extends MIP.CustomElement {
-  build() {
-    let tokens = this.element.getAttribute('token');
+  build () {
+    let tokens = this.element.getAttribute('token')
 
     tokens.split(' ').forEach(token => {
-      let elem = document.createElement('mip-stats-baidu');
-      elem.setAttribute('token', config[token]);
-      this.element.appendChild(elem);
+      let elem = document.createElement('mip-stats-baidu')
+      elem.setAttribute('token', config[token])
+      this.element.appendChild(elem)
     })
   }
 }
