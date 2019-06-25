f98389f3acced0c7cf4a1f6b00b7dc80022e5cf2
liushaokai <liushaokai@yuanxin-inc.com>
2019-06-25 16:19:54 +0800

    组件升级

--- sites/miaoshou.net/components/mip-stats-miaoshou-v2/mip-stats-miaoshou-v2.js
+++ sites/miaoshou.net/components/mip-stats-miaoshou-v2/mip-stats-miaoshou-v2.js
@@ -6,26 +6,23 @@ const { CustomElement } = MIP
 const { fetchJsonp } = window
 
 export default class MIPStatsMiaoshouV2 extends CustomElement {
-    build () {
-        let e = this.element;
-        let url = e.getAttribute('url');
-        let tid = e.getAttribute('tid');
-        let type = e.getAttribute('type');
-        let selectName = e.getAttribute('selectName') ? e.getAttribute('selectName') : '#view_num';
-
-        url = url + '?id=' + tid + '&type=' + type;
-
-        fetchJsonp(url, {
-            jsonpCallback: 'cb',
-            timeout: 5000
-        }).then(function (res) {
-            return res.json();
-        }).then(function (data) {
-            if (data.views) {
-                let dom = e.querySelector(selectName);
-                dom.innerHTML = data.views + '人阅读';
-            }
-        });
-
-    }
+  build () {
+    let e = this.element;
+    let url = e.getAttribute('url');
+    let tid = e.getAttribute('tid');
+    let type = e.getAttribute('type');
+    let selectName = e.getAttribute('selectName') ? e.getAttribute('selectName') : '#view_num';
+    url = url + '?id=' + tid + '&type=' + type;
+    fetchJsonp(url, {
+        jsonpCallback: 'cb',
+        timeout: 5000
+    }).then(function (res) {
+        return res.json();
+    }).then(function (data) {
+        if (data.views) {
+            let dom = e.querySelector(selectName);
+            dom.innerHTML = data.views + '人阅读';
+        }
+    });
+  }
 }
