3413c2a521d6d925618209e73645c49c84363feb
wangsha <wangsha@mail.china.cn>
2019-07-08 16:03:41 +0800

    修改代码规范

--- sites/china.cn/components/mip-chinacn-getvideourl/mip-chinacn-getvideourl.js
+++ sites/china.cn/components/mip-chinacn-getvideourl/mip-chinacn-getvideourl.js
@@ -7,29 +7,27 @@ import md5 from './md5'
 
 export default class MIPChinacnGetvideourl extends MIP.CustomElement {
   firstInviewCallback () {
-    let element = this.element;
-    let myVideo = element.querySelector('video');
-    let request = false;
+    let element = this.element
+    let myVideo = element.querySelector('video')
+    let request = false
     myVideo.addEventListener('play', function () {
-      if (request === true) {
-        return;
-      } else {
-        myVideo.pause();
-        let id = element.querySelector('#id').value;
-        let k = md5('chinavideoplay');
+      if (request === false) {
+        myVideo.pause()
+        let id = element.querySelector('#id').value
+        let k = md5('chinavideoplay')
         fetch('https://www.china.cn/video_api.php?a=play&k=' + k + '&t=mip&id=' + id + '')
           .then(function (response) {
-            return response.json();
+            return response.json()
           })
           .then(function (myJson) {
-            request = true;
-            let videoUrl = myJson.data;
-            myVideo.setAttribute('src', videoUrl);
-            myVideo.play();
+            request = true
+            let videoUrl = myJson.data
+            myVideo.setAttribute('src', videoUrl)
+            myVideo.play()
           }).catch(function (e) {
-            console.log(e);
-          });
+            console.log(e)
+          })
       }
-    });
+    })
   }
 }
