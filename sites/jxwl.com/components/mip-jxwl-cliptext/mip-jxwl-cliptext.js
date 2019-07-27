4d16710adc1cd9411f7e0ca56c2d4288bad829a8
mip-platform <mip-platform@baidu.com>
2019-07-27 11:01:45 +0800

    mip-platform auto create Pull Request

--- sites/jxwl.com/components/mip-jxwl-cliptext/mip-jxwl-cliptext.js
+++ sites/jxwl.com/components/mip-jxwl-cliptext/mip-jxwl-cliptext.js
@@ -1,6 +1,7 @@
 import './mip-jxwl-cliptext.less'
 
 const { util } = MIP
+const { platform } = util
 
 export default class MIPJxwlClipText extends MIP.CustomElement {
   build () {
@@ -20,6 +21,7 @@ export default class MIPJxwlClipText extends MIP.CustomElement {
       oInput.value = copytext.innerHTML
       document.body.appendChild(oInput)
       oInput.select()
+      oInput.setSelectionRange(0, oInput.value.length)  
       let success = document.execCommand('copy')
       document.body.removeChild(oInput)
       if (success) {
