3269dbc355d137d5c1612f79c296e004a19c5add
shuxingxu <905336308@qq.com>
2019-07-29 09:49:57 +0800

    规范修改

--- sites/fh21.com.cn/components/mip-video-over/mip-video-over.js
+++ sites/fh21.com.cn/components/mip-video-over/mip-video-over.js
@@ -15,7 +15,7 @@ export default class MIPVideoOver extends MIP.CustomElement {
         let video = document.createElement('video');
 
         // 创建视频
-        function createVideo(){
+        function createVideo () {
         	let _w = 16;
         	let _h = 9;
         	video.src = src;
@@ -26,30 +26,30 @@ export default class MIPVideoOver extends MIP.CustomElement {
         	addVideoAttr(controls, 'controls');
         	addVideoAttr(preload, 'preload');
 
-        	if(wh){
-        		_w = wh.split('wh')[0];
-        		_h = wh.split('wh')[1];
-        	}
+        	if (wh) {
+        	    _w = wh.split('wh')[0];
+        	    _h = wh.split('wh')[1];
+            }
 
-        	let w = ele.offsetWidth || window.innerWidth;
+            let w = ele.offsetWidth || window.innerWidth;
 
-        	let height = w / _w * _h + 'px';
-        	video.style.height = height;
+            let height = w / _w * _h + 'px';
+            video.style.height = height;
 
-        	ele.appendChild(video)
+            ele.appendChild(video);
         }
 
-        function addVideoAttr(v, a){
-        	if(v) video[a] = v;
+        function addVideoAttr (v, a) {
+            if(v) video[a] = v;
         }
 
         // 初始视频
         createVideo();
 
-        //  当播放开始的时候设置为自动播放
+        // 当播放开始的时候设置为自动播放
         video.onplay = function () {
             popEle.style.display = 'none';
-        	addVideoAttr(controls, 'controls');
+            addVideoAttr(controls, 'controls');
         };
 
         // 视频播放完成
@@ -58,15 +58,15 @@ export default class MIPVideoOver extends MIP.CustomElement {
             video.controls = false;
         };
 
-	    // 重新播放
-	    this.addEventAction('play', (event, str) => {
-	      video.play();
-	    })
+        // 重新播放
+        this.addEventAction('play', (event, str) => {
+            video.play();
+        })
         // 关闭弹窗
-	    this.addEventAction('close', (event, str) => {
-	      popEle.style.display = 'none';
-	      video.controls = true;
-	    })
-    
+        this.addEventAction('close', (event, str) => {
+            popEle.style.display = 'none';
+            video.controls = true;
+        })
+
   	}
 }
