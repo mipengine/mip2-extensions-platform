1150d45290e81a936b61dff1a8e75da7e7fb0973
fangle1212 <592825310@qq.com>
2019-07-31 14:35:27 +0800

    增加id区分内容

--- sites/m.pc6.com/common/mip-common.js
+++ sites/m.pc6.com/common/mip-common.js
@@ -110,7 +110,7 @@ export default class MIPCommon {
       for (let i = 0; i < bodyCa.length; i++) {
         bodyCa[i].innerHTML = caName
       }
-      // 动态显示不同内容
+      // 动态显示区分内容
       let bodyDif = document.getElementsByClassName('dif')
       for (let i = 0; i < bodyDif.length; i++) {
         if(aiJson.index != undefined){
