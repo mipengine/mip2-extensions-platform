cefac7bf7c05bfc64cf7378ed6ef147a5e1424f9
kong_kong <kong_kong@hunliji.com>
2019-08-01 16:03:10 +0800

    百科详情

---
+++ sites/hunliji.com/components/mip-hlj-baike/mip-hlj-baike.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+
+export default {
+  mounted () {
+    // 套餐
+    const workElements = this.$element.querySelectorAll('.workStyle')
+    console.log('workElements', workElements, this.$element.querySelector('.workStyle.work1'))
+    for (let i=0; i< workElements.length; i++) {
+      workElements[i].onclick = (e) => {
+        let id = ''
+        if (workElements[i].querySelector('.dtImg')) {
+          id = parseInt(workElements[i].querySelector('.dtImg').className.substring(6))
+        }
+        const className = workElements[i].className
+
+        console.log(className.indexOf(' work') > -1, i, className)
+
+        if (className.indexOf(' work') > -1) {
+          MIP.viewer.open('//m.hunliji.com/package/detail_' + id)
+        } else if (className.indexOf('case') > -1) {
+          MIP.viewer.open('//m.hunliji.com/example/detail_' + id)
+        } else if (className.indexOf('merchant') > -1) {
+          MIP.viewer.open('//m.hunliji.com/merchant/detail_' + id)
+        } else if (className.indexOf('hotel') > -1) {
+          id = workElements[i].querySelector('.hotelTit mip-img').className
+          MIP.viewer.open('//m.hunliji.com/merchant/detail_' + parseInt(id))
+        } else if (className.indexOf('weddingBig') > -1) {
+          MIP.viewer.open('//www.hunliji.com/hunpin/detail_' + id)
+        }
+      }
+    }
+
+
+
+  }
+}
+</script>
