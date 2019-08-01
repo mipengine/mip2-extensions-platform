0ed5d0bdda3868080109778578aaa13beb14102b
mip-platform <mip-platform@baidu.com>
2019-08-01 09:27:32 +0800

    mip-platform auto create Pull Request

---
+++ sites/mip.ppkao.com/components/mip-ppkao-buyqiehuan/mip-ppkao-buyqiehuan.js
@@ -0,0 +1,22 @@
+/**
+ * @file mip-ppkao-buyqiehuan 组件
+ * @author
+ */
+export default class MIPExample extends MIP.CustomElement {
+  build () {
+    let $ = require('zepto')
+    let customElement = require('customElement').create()
+
+    /**
+     * 第一次进入可视区回调，只会执行一次
+     */
+    customElement.prototype.firstInviewCallback = function () {
+      let ele = $(this.element)
+      let chooseLi = ele.find('.choose-vip ul li')
+      chooseLi.click(function () {
+        $(this).addClass('check').siblings().removeClass('check')
+      })
+    }
+    return customElement
+  }
+}
