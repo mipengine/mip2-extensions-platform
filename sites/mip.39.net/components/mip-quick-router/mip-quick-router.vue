3995548ab8b2d97826cd29ff3c17a9aa0c4b699f
jiangzhuorong <490356762@qq.com>
2019-08-01 13:49:33 +0800

    新增快应用调起组件

--- sites/mip.39.net/components/mip-quick-router/mip-quick-router.vue
+++ sites/mip.39.net/components/mip-quick-router/mip-quick-router.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="wrapper">
-    <iframe v-if="iframeSrc" v-bind:src="iframeSrc" style="display: none;"></iframe>
-    <slot></slot>
+    <iframe
+      v-if="iframeSrc"
+      :src="iframeSrc"
+      class="hidden"
+    />
   </div>
 </template>
 
 <style scoped>
-  .fucking-pic{display:none;}
+  .hidden{display:none;}
 </style>
 
 <script>
@@ -18,25 +21,25 @@ export default {
         return ''
       }
     },
-    appRouter:{
+    appRouter: {
       type: Boolean,
       default () {
         return true
       }
     },
-    page:{
+    page: {
       type: String,
       default () {
         return ''
       }
     },
-    params:{
+    params: {
       type: String,
       default () {
         return ''
       }
     },
-    protocal:{
+    protocal: {
       type: String,
       default () {
         return 'http'
@@ -45,103 +48,98 @@ export default {
   },
   data () {
     return {
-      iframeSrc:''
+      iframeSrc: ''
     }
   },
 
-  methods:{
-    convertToHash(str) {
-      var hash = {};
-      var list = str.split('&');
-      for (var i = 0, len = list.length; i < len; i++) {
-          var item = list[i];
-          var sideList = item.split('=');
-          if (sideList[0].length > 0) {
-              hash[sideList[0]] = item.substr(sideList[0].length + 1);
-          }
-      }
-      return hash;
-    },
-    typeofHuawei(){
-        var userAgent = navigator.userAgent.toLowerCase();
-        console.log(userAgent)
-        if (userAgent) {
-            var huaweibrowser = userAgent.indexOf("huaweibrowser");
-            var android = userAgent.indexOf("android")
-            var huawei = userAgent.indexOf("build/huawei");
-            if (huaweibrowser >= 0) return 1;
-            if (huawei >= 0) return 1;
-            // if (0 > huawei && (huawei = userAgent.indexOf("build/honor")), android >= 0 && huawei >= 0) {
-            //     if (userAgent.slice(android + 8, android + 9) >= 8) return 0
-            // }
-        }
-        return 0
-    },
-    isEmptyObject(obj) {
-      if (!obj) {
-          return !0;
-      }
-      var t = void 0;
-      for (t in obj) {
-          if (obj.hasOwnProperty(t)) {
-              return !1;
-          }
-      }
-      return !0;
-    }
-  },
-  
-  created(){
-    var packagename = this.packagename;
-    var protocal = this.protocal
-    var page = this.page
-    var params = this.convertToHash(this.params)
-    var appRouter = !!this.appRouter
-    var query
+  created () {
+    let packagename = this.packagename
+    let protocal = this.protocal
+    let page = this.page
+    let params = this.convertToHash(this.params)
+    let appRouter = !!this.appRouter
+    let query
 
-    
-    if(appRouter){
-      let iframeSrc = '';
+    if (appRouter) {
+      let iframeSrc = ''
 
       if (this.isEmptyObject(params)) {
-        var url = window.location.search;
-        var index = url.indexOf('?');
+        let url = window.location.search
+        let index = url.indexOf('?')
         if (index > -1) {
-            query = url.substr(1);
+          query = url.substr(1)
         }
-      }else {
-        var items = Object.keys(params).map(function (key) {
-            return key + '=' + encodeURIComponent(params[key]);
-        });
-        query = items.join('&');
+      } else {
+        let items = Object.keys(params).map(function (key) {
+          return key + '=' + encodeURIComponent(params[key])
+        })
+        query = items.join('&')
       }
 
-      if(this.typeofHuawei()){
-        iframeSrc = 'hap://app/'+packagename 
+      if (this.typeofHuawei()) {
+        iframeSrc = 'hap://app/' + packagename
         if (page) {
-          iframeSrc+= '/' + page
+          iframeSrc += '/' + page
         }
 
         if (query) {
-            iframeSrc +=   '?' + query
+          iframeSrc += '?' + query
         }
-      }else{
-        iframeSrc = protocal +'://thefatherofsalmon.com?i='+packagename 
+      } else {
+        iframeSrc = protocal + '://thefatherofsalmon.com?i=' + packagename
         if (page) {
-          iframeSrc+= '&p='+page
+          iframeSrc += '&p=' + page
         }
 
         if (query) {
-            iframeSrc +=  '&a=' + encodeURIComponent(query);
+          iframeSrc += '&a=' + encodeURIComponent(query)
         }
       }
 
-          
       this.iframeSrc = iframeSrc
-
     }
-    
-   
+  },
+
+  methods: {
+    convertToHash (str) {
+      let hash = {}
+      let list = str.split('&')
+      for (let i = 0, len = list.length; i < len; i++) {
+        let item = list[i]
+        let sideList = item.split('=')
+        if (sideList[0].length > 0) {
+          hash[sideList[0]] = item.substr(sideList[0].length + 1)
+        }
+      }
+      return hash
+    },
+    typeofHuawei () {
+      let userAgent = navigator.userAgent.toLowerCase()
+      console.log(userAgent)
+      if (userAgent) {
+        let huaweibrowser = userAgent.indexOf('huaweibrowser')
+        //let android = userAgent.indexOf('android')
+        let huawei = userAgent.indexOf('build/huawei')
+        if (huaweibrowser >= 0) return 1
+        if (huawei >= 0) return 1
+        // if (0 > huawei && (huawei = userAgent.indexOf("build/honor")), android >= 0 && huawei >= 0) {
+        //     if (userAgent.slice(android + 8, android + 9) >= 8) return 0
+        // }
+      }
+      return 0
+    },
+    isEmptyObject (obj) {
+      if (!obj) {
+        return !0
+      }
+      let t = void 0
+      for (t in obj) {
+        if (obj.hasOwnProperty(t)) {
+          return !1
+        }
+      }
+      return !0
+    }
   }
 }
 </script>
