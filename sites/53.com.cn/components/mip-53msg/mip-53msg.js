d402fddab793b151b638798d58df26516939bbe2
mip-platform <mip-platform@baidu.com>
2019-08-02 16:17:47 +0800

    mip-platform auto create Pull Request

--- sites/53.com.cn/components/mip-53msg/mip-53msg.js
+++ sites/53.com.cn/components/mip-53msg/mip-53msg.js
@@ -1,97 +1,104 @@
 import './index.less'
-
 export default class MIPExample extends MIP.CustomElement {
   build () {
-    var nick = this.element.getAttribute("popContact"),
-		tel = this.element.getAttribute("popTel"),
-		con = this.element.getAttribute("popInfo"),
-		but = this.element.getAttribute("popButton"),
-    tid = this.element.getAttribute("projectId");
-    var vm = this;
-    document.getElementById(but).onclick = function(){
-      var name = document.getElementById(nick).innerText.replace(/\s*/g,""),
-        phone = document.getElementById(tel).innerText.replace(/\s*/g,""),
-        conten = document.getElementById(con).innerText.replace(/\s*/g,""),
-        isphone = /^0?1[3|4|5|7|8|9][0-9]\d{8}$/;
-      if(nick !== ''){
-        if (name === ""){
-          document.getElementById(nick).setAttribute("placeholder","请输入姓名")
+    let nick = this.element.getAttribute('popContact')
+		let  tel = this.element.getAttribute('popTel')
+		let  con = this.element.getAttribute('popInfo')
+		let  but = this.element.getAttribute('popButton')
+    let tid = this.element.getAttribute('projectId')
+    let laveMsg = this.element.getAttribute('laveMsg')
+    let vm = this
+    document.getElementById(but).onclick = function () {
+      let name = document.getElementById(nick).innerText.replace(/\s*/g,'')
+      let phone = document.getElementById(tel).innerText.replace(/\s*/g,'')
+      let conten = document.getElementById(con).innerText.replace(/\s*/g,'')
+      let isphone = /^0?1[3|4|5|7|8|9][0-9]\d{8}$/
+      if (nick !== '') {
+        if (name === '') {
+          document.getElementById(nick).setAttribute('placeholder','请输入姓名')
           document.getElementById(nick).style.borderColor='rgb(255, 0, 0)'
-          return;
-        }else{
+          return
+        } else {
           document.getElementById(nick).style.borderColor='rgb(210, 210, 210)'
         }
-        if (phone === ""){
-          document.getElementById(tel).setAttribute("placeholder","请输入电话")
+        if (phone === "") {
+          document.getElementById(tel).setAttribute('placeholder','请输入电话')
           document.getElementById(tel).style.borderColor='rgb(255, 0, 0)'
-          return;
+          return
         }
-        if(!isphone.test(phone)){
+        if(!isphone.test(phone)) {
           document.getElementById(tel).innerText = ''
-          document.getElementById(tel).setAttribute("placeholder","请输入正确的联系方式")
+          document.getElementById(tel).setAttribute('placeholder','请输入正确的联系方式')
           document.getElementById(tel).style.borderColor='rgb(255, 0, 0)'
-          return;
-        }else{
+          return
+        }else {
           document.getElementById(tel).style.borderColor='rgb(210, 210, 210)'
         }
-        var MeData = vm.GetIsTuiguang(tid,conten)
-        console.log(MeData)
-        //提交参数
-        var option = {
+        let MeData = vm.GetIsTuiguang(tid,conten)
+        // 提交参数
+        let option = {
           tid: MeData.Pid,
-          type: "addMsg",
+          type: 'addMsg',
           tel: phone,
           popInfo: MeData.MeInfo,
           popContact: name
         }
-        
-        // vm.submitMessage(option);//提交请求
-        var AJAXurl = `https://m.53.com.cn/common/mipwebdo.ashx?tid=${option.tid}&type=${option.type}&tel=${option.tel}&popInfo=${option.popInfo}&popContact=${option.popContact}`
-        let req = new Request(AJAXurl, {
-          headers: new Headers({
-            'Accept': 'application/json',
-            'Content-Type': 'application/x-www-form-urlencoded'
-          }),
+
+        // 提交请求
+        let AJAXurl = 'https://m.53.com.cn/common/mipwebdo.ashx'
+        let AJAXdata = `tid=${option.tid}&type=${option.type}&tel=${option.tel}&popInfo=${option.popInfo}&popContact=${option.popContact}`
+        fetch(AJAXurl, {
           method: 'POST',
-          cache: 'reload',
-        })
-        fetch(req).then(function (response) {
+        	headers:{
+        			'Content-Type': 'application/x-www-form-urlencoded'
+        	},
+          body: AJAXdata
+        }).then(function (response){
           return response.json()
         }).then(function (res) {
-          // 成功回调
-          document.getElementById(nick).innerHTML = "";
-          document.getElementById(tel).innerHTML = "";
-          document.getElementById(con).innerHTML = "";
-          if (res){
-            console.log('留言成功')
-          }else{
-            console.log('留言失败3')
+          console.log(res);
+          if (res !== '') {
+            document.getElementById(nick).innerHTML = ''
+            document.getElementById(tel).innerHTML = ''
+            document.getElementById(con).innerHTML = ''
+            document.getElementById(laveMsg).innerHTML = '留言成功!'
           }
-        })
+        }, function (error) {})
+        // let AJAXurl = `https://m.53.com.cn/common/mipwebdo.ashx?tid=${option.tid}&type=${option.type}&tel=${option.tel}&popInfo=${option.popInfo}&popContact=${option.popContact}` 
+        // let req = new Request(AJAXurl, {
+        //   headers: new Headers({
+        //     'Accept': 'application/json',
+        //     'Content-Type': 'application/x-www-form-urlencoded'
+        //   }),
+        //   method: 'POST',
+        //   cache: 'reload',
+        // })
+        // fetch(req).then(function (response) {
+        //   return response.json()
+        // }).then(function (res) {
+        //   // 成功回调
+        //   document.getElementById(nick).innerHTML = ""
+        //   document.getElementById(tel).innerHTML = ""
+        //   document.getElementById(con).innerHTML = ""
+        //   if (res) {
+        //     console.log('留言成功')
+        //   }else {
+        //     console.log('留言失败3')
+        //   }
+        // })
       }
-
     }
   }
-  GetIsTuiguang(pid, meinfo) {
-    var Data = new Object();
-    var lsvalue = localStorage.getItem("IsTuiguang");
+  GetIsTuiguang (pid, meinfo) {
+    let Data = new Object()
+    let lsvalue = localStorage.getItem("IsTuiguang") 
     if (lsvalue) {
-      Data.Pid = 2944621;
-      Data.MeInfo = lsvalue;
+      Data.Pid = 2944621
+      Data.MeInfo = lsvalue
+    } else {
+      Data.Pid = pid
+      Data.MeInfo = meinfo
     }
-    else {
-      Data.Pid = pid;
-      Data.MeInfo = meinfo;
-    }
-    return Data;
+    return Data
   }
-  //请求方法
-  submitMessage(option){
-    var AJAXurl = `https://m.53.com.cn/common/mipwebdo.ashx?tid=${option.tid}&type=${option.type}&tel=${option.tel}&popInfo=${option.popInfo}&popContact=${option.popContact}`
-    fetch(AJAXurl).then(function (response) {
-      return response.json()
-    }).then(function (res) {
-      console.log(res);
-    })
-  }
-}
+} 
--- sites/53.com.cn/components/mip-53public/mip-53public.js
+++ sites/53.com.cn/components/mip-53public/mip-53public.js
@@ -1,26 +1,24 @@
 import './index.less'
-
 export default class MIP53public extends MIP.CustomElement {
   build () {
-    // var hrefs = 'https://mip.53.com.cn?hmsr=baidu&hmpl=&hmcu=&hmkw=&hmci='
-    var hrefs = window.location.href
-    var IsTuiguang = this.getParamArgs(hrefs,'hmsr')
-    localStorage.setItem("IsTuiguang", IsTuiguang)
+    // let hrefs = 'https://mip.53.com.cn?hmsr=baidu&hmpl=&hmcu=&hmkw=&hmci=' 
+    let hrefs = window.location.href
+    let IsTuiguang = this.getParamArgs(hrefs, 'hmsr')
+    localStorage.setItem('IsTuiguang', IsTuiguang)
   }
-  //获取链接中包含的hmsr参数
+  // 获取链接中包含的hmsr参数 
   getParamArgs(parms, parmName) {
-    var argIndex = parms.indexOf("?");
-    var arg = parms.substring(argIndex + 1);
-    var valArg = "";
-    var args = arg.split("&");
-    for (var i = 0; i < args.length; i++) {
-        var str = args[i];
-        var arg = str.split("=");
-        if (arg.length <= 1) continue;
-        if (arg[0] == parmName)
-            valArg = arg[1];
+    let argIndex = parms.indexOf('?')
+    let arg = parms.substring(argIndex + 1)
+    let valArg = ''
+    let args = arg.split('&')
+    for (let i = 0; i < args.length; i++) {
+      let str = args[i]
+      let arg = str.split('=')
+      if (arg.length <= 1) continue
+      if (arg[0] === parmName)
+        valArg = arg[1]
     }
-    return valArg;
-  }
-
-}
+    return valArg
+  } 
+} 
