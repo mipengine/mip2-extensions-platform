f9a4f2fd26bb383136203d46cdc6c1163d791c0f
mip-platform <mip-platform@baidu.com>
2019-08-03 09:30:19 +0800

    mip-platform auto create Pull Request

--- sites/jxwl.com/components/mip-jxwl-cliptext/mip-jxwl-cliptext.js
+++ sites/jxwl.com/components/mip-jxwl-cliptext/mip-jxwl-cliptext.js
@@ -8,23 +8,19 @@ export default class MIPJxwlClipText extends MIP.CustomElement {
     let copybox = el.querySelector('.copy')
     let copybtn = copybox.querySelector('em')
     let copytext = copybox.querySelector('p')
-    //移动端实际并没有mouseover和mouseout组件，实际效果就是点击出现和消失
-    //点击黑框显示复制按钮
     copybox.addEventListener('mouseover', e => {
       util.css(copybtn, 'display', 'inline')
       copybtn.innerHTML = '复制'
     })
-    //点击黑框外隐藏复制按钮
     copybox.addEventListener('mouseout', e => {
       util.css(copybtn, 'display', 'none')
     })
-    //复制按钮点击事件
     copybtn.addEventListener('click', e => {
       let oInput = document.createElement('input')
       oInput.value = copytext.innerHTML
       document.body.appendChild(oInput)
       oInput.select()
-      oInput.setSelectionRange(0, oInput.value.length) //这句是兼容ios的重点
+      oInput.setSelectionRange(0, oInput.value.length)
       let success = document.execCommand('copy')
       document.body.removeChild(oInput)
       if (success) {
--- sites/jxwl.com/components/mip-jxwl-showdetail/mip-jxwl-showdetail.js
+++ sites/jxwl.com/components/mip-jxwl-showdetail/mip-jxwl-showdetail.js
@@ -5,12 +5,12 @@ export default class MIPJxwlShowDetail extends MIP.CustomElement {
     let showbox = el.querySelector('.qlist_intromore')
     let textcon = textbox.innerHTML
     let limit = el.getAttribute('limit')
-    if (textcon.length>limit) {
-      let cuttext = textcon.substr(0,limit)+' ... '
+    if (textcon.length > limit) {
+      let cuttext = textcon.substr(0, limit) + ' ... '
       textbox.innerHTML = cuttext
       showbox.innerHTML = '&nbsp;&nbsp;详情'
       showbox.addEventListener('click', function (e) {
-        if ( this.classList.contains('zk') ) {
+        if (this.classList.contains('zk')) {
           textbox.innerHTML = cuttext
           this.classList.remove('zk')
           this.innerHTML = '&nbsp;&nbsp;详情'
@@ -19,7 +19,7 @@ export default class MIPJxwlShowDetail extends MIP.CustomElement {
           this.classList.add('zk')
           this.innerHTML = '&nbsp;&nbsp;收起'
         }
-      });
+      })
     }
   }
 }
