00b00e6cf9613deb33e0024c0313566d872e53f1
ex-wife <53594677+ex-wife@users.noreply.github.com>
2019-08-05 18:16:19 +0800

    自定义自动计时打开模态层

---
+++ sites/wangshaoguo.cn/components/mip-wangshaoguocn-automodal/mip-wangshaoguocn-automodal.js
@@ -0,0 +1,26 @@
+export default class MipWangshaoguocnAutomodal extends MIP.CustomElement {
+	 build () {
+	    var swtBox = document.getElementById("swtBox");
+	    
+		var timer_1 = setTimeout(function(){
+				swtBox.style.display = "block";
+			},3000);
+		
+		function showSwtBox(){
+			clearTimeout(timer_2);
+			var timer_2 = setTimeout(function(){
+				swtBox.style.display = "block";
+			},3000);
+		};
+		
+		swtBox.firstElementChild.addEventListener("click",function(){
+			swtBox.style.display = "none";
+			showSwtBox();
+		});
+		
+		swtBox.lastElementChild.addEventListener("click",function(){
+			swtBox.style.display = "none";
+			showSwtBox();
+		});
+	};
+}
