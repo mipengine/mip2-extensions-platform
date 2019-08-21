85806c04fedd05b78b2959dbdaa15333bfeed146
sunchao <sunchao@66law.cn>
2019-08-21 14:20:20 +0800

    fix(components):修复JS异味

--- sites/mip.66law.cn/components/mip-geolocation/mip-geolocation.js
+++ sites/mip.66law.cn/components/mip-geolocation/mip-geolocation.js
@@ -1,14 +1,14 @@
 const { fetchJsonp } = window
 export default class MIPGeolocation extends MIP.CustomElement {
-    constructor (element){
-        super(element)
-        this.src = '';
-        this.timeout =50000;
-    }  
-    build () {
-     let attributes = this.element.attributes;
-     if(attributes && attributes.length>0)
-     {
+  constructor (element) {
+    super(element)
+    this.src = ''
+    this.timeout = 50000
+  }
+  build () {
+    let attributes = this.element.attributes
+    if (attributes && attributes.length > 0)
+    {
         for(var i=0;i<attributes.length;i++)
         {
             var name=attributes[i].name;
@@ -21,10 +21,10 @@ export default class MIPGeolocation extends MIP.CustomElement {
                 this.timeout=attributes[i].value;
             }
         }
-     }
-     
-     if(this.src && this.src.length>0)
-     {
+    }
+    
+    if(this.src && this.src.length>0)
+    {
         let cookieValue = this.getCookie('acoks');
         if(!cookieValue || cookieValue!='1')
         {
@@ -47,19 +47,19 @@ export default class MIPGeolocation extends MIP.CustomElement {
             },function(error){
                 switch(error.code){
                     case error.TIMEOUT :
-                        console.log('连接超时，请重试');
-                        break;
+                         console.log('连接超时，请重试');
+                         break;
                     case error.PERMISSION_DENIED :
-                        console.log('您拒绝了使用位置共享服务，查询已取消');
-                        break;
+                         console.log('您拒绝了使用位置共享服务，查询已取消');
+                         break;
                     case error.POSITION_UNAVAILABLE : 
-                        console.log('亲爱的火星网友，非常抱歉，我们暂时无法为您所在的星球提供位置服务');
-                        break;
+                         console.log('亲爱的火星网友，非常抱歉，我们暂时无法为您所在的星球提供位置服务');
+                         break;
                 }    
             });
         }
-     }
-     
+    }
+    
     }
     
     /*获取Cookie*/
