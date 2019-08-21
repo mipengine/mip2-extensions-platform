bc71ff0ad38ef67b7bb1211288035b2a8bc1c6ff
sunchao <sunchao@66law.cn>
2019-08-21 18:47:54 +0800

    fix(components):修复JS异味

--- sites/mip.66law.cn/components/mip-geolocation/mip-geolocation.js
+++ sites/mip.66law.cn/components/mip-geolocation/mip-geolocation.js
@@ -7,73 +7,61 @@ export default class MIPGeolocation extends MIP.CustomElement {
   }
   build () {
     let attributes = this.element.attributes
-    if (attributes && attributes.length > 0)
-    {
-        for(var i=0;i<attributes.length;i++)
-        {
-            var name=attributes[i].name;
-            if(name=='src')
-            {
-                this.src=attributes[i].value;
+    if (attributes && attributes.length > 0) {
+      for(let i=0; i<attributes.length; i++) {
+        let name=attributes[i].name;
+        if(name=='src') {
+          this.src=attributes[i].value;
+        }
+        else if(name=='timeout') {
+          this.timeout=attributes[i].value;
+        }
+      }
+    }
+    
+    if(this.src && this.src.length>0) {
+      let cookieValue = this.getCookie('acoks');
+        if(!cookieValue || cookieValue!='1') {
+          /**
+           * GPS H5定位
+           */
+          navigator.geolocation.getCurrentPosition(function(position){
+            let lat = position.coords.latitude;
+            let lng = position.coords.longitude;
+            let url=this.src;
+            if(url.indexOf('?')) {
+              url=url+"&lat="+lat+"&lon="+lng;
             }
-            else if(name=='timeout')
-            {
-                this.timeout=attributes[i].value;
+            else {
+             url=url+"?lat="+lat+"&lon="+lng;
             }
+            fetchJsonp(url, { timeout: timeout });
+          }
+		  ,function(error) {
+            switch(error.code) {
+              case error.TIMEOUT :
+                console.log('连接超时，请重试');
+                break;
+              case error.PERMISSION_DENIED :
+                console.log('您拒绝了使用位置共享服务，查询已取消');
+                break;
+              case error.POSITION_UNAVAILABLE : 
+                console.log('亲爱的火星网友，非常抱歉，我们暂时无法为您所在的星球提供位置服务');
+                break;
+            }    
+          });
         }
     }
-    
-    if(this.src && this.src.length>0)
-    {
-        let cookieValue = this.getCookie('acoks');
-        if(!cookieValue || cookieValue!='1')
-        {
-            /**
-             * GPS H5定位
-             */ 
-            navigator.geolocation.getCurrentPosition(function(position){
-                let lat = position.coords.latitude;
-                let lng = position.coords.longitude;
-                var url=this.src;
-                if(url.indexOf('?'))
-                {
-                    url=url+"&lat="+lat+"&lon="+lng;
-                }
-                else
-                {
-                    url=url+"?lat="+lat+"&lon="+lng;
-                }
-                fetchJsonp(url, { timeout: timeout });
-            },function(error){
-                switch(error.code){
-                    case error.TIMEOUT :
-                         console.log('连接超时，请重试');
-                         break;
-                    case error.PERMISSION_DENIED :
-                         console.log('您拒绝了使用位置共享服务，查询已取消');
-                         break;
-                    case error.POSITION_UNAVAILABLE : 
-                         console.log('亲爱的火星网友，非常抱歉，我们暂时无法为您所在的星球提供位置服务');
-                         break;
-                }    
-            });
-        }
-    }
-    
-    }
-    
+  } 
     /*获取Cookie*/
-    getCookie(name)
-    {
-        let reg = new RegExp("(^| )"+name+"=([^;]*)(;|$)");
-        let arr=document.cookie.match(reg);
-        if(arr)
-        {
-            return unescape(arr[2]); 
-        }
-        else
-        {
-            return null;
-        }
+  getCookie(name) {
+    let reg = new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+    let arr=document.cookie.match(reg);
+    if(arr) {
+      return unescape(arr[2]); 
     }
+    else {
+      return null;
+    }
+  }
 }
