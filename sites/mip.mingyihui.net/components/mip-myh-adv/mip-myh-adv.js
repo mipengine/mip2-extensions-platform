3203ffdfadde24f0880326698cbe54a33b3e9d33
jonny jerry <869064381@qq.com>
2019-08-22 11:30:05 +0800

    组件优化 支持组合广告

--- sites/mip.mingyihui.net/components/mip-myh-adv/mip-myh-adv.js
+++ sites/mip.mingyihui.net/components/mip-myh-adv/mip-myh-adv.js
@@ -7,9 +7,9 @@ export default class MIPMYHAdv extends MIP.CustomElement {
   build () {
     // 在这里注册 say 事件的监听
     this.addEventAction('click', (event, adSign) => {
-      this.faKey(adSign, 1)
       let adSignVal = adSign + ',' + this.postData['userid'] + ',' + this.postExtParams['referer'] + ',' + (new Date()).getTime()
       setCookie('_adv_invite', adSignVal)
+      this.faKey(adSign, 1)
     })
   }
   connectedCallback () {
@@ -81,15 +81,26 @@ export default class MIPMYHAdv extends MIP.CustomElement {
     if (data.adv_list.length === 0) return returnData
     data.adv_list.forEach(adv => {
       this.faKey(adv['sign'], 0)
-      adv.ad_contents[0]['ad_sign'] = adv['sign']
-      returnData.push(adv.ad_contents[0])
+        let ad_contents = []
+        adv.ad_contents.forEach(ad_content=>{
+          if (ad_contents.length === 0) {
+            ad_contents=ad_content
+            ad_contents['ad_name'] = adv['ad_name']
+            ad_contents['ad_sign'] = adv['sign']
+            ad_contents['lists']=[]
+          } else {
+            ad_contents['lists'].push(ad_content)
+          }
+        })
+      returnData.push(ad_contents)
     })
+    // console.log(returnData)
     return returnData
   }
 
   faKey (adSign, mode) {
     let keyName = this.postExtParams['referer'] + 'Adv_' + adSign + (mode === 0 ? '_View' : '_Click')
-    let statUrl = 'http://www.mingyihui.net/api/stat/statKey2/' + keyName + '/0/' + this.postData['userid']
+    let statUrl = 'https://www.mingyihui.net/api/stat/statKey2/' + keyName + '/0/' + this.postData['userid']
     return fetch(statUrl, {
     })
   }
