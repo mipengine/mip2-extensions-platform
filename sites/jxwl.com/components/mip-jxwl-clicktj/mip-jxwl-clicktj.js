12b56098a2e034bfb07d3e718174ff455236d524
mip-platform <mip-platform@baidu.com>
2019-09-07 11:50:11 +0800

    mip-platform auto create Pull Request

---
+++ sites/jxwl.com/components/mip-jxwl-clicktj/mip-jxwl-clicktj.js
@@ -0,0 +1,18 @@
+export default class MIPJxwlClickTj extends MIP.CustomElement {
+  build () {
+    let req = new Request("https://api.youxigt.com/ajax/hits/type/click", {
+      'mode':'no-cors'
+    })
+    fetch(req).then(function() {})
+    let az = document.getElementById('android')
+    let ios = document.getElementById('ios')
+    az.addEventListener('click', e => {
+      let aurl = az.getAttribute('href')
+      fetch("https://api.youxigt.com/ajax/hits/type/down?url="+aurl).then(function() {})
+    })
+    ios.addEventListener('click', e => {
+      let iurl = ios.getAttribute('href')
+      fetch("https://api.youxigt.com/ajax/hits/type/down?url="+iurl).then(function() {})
+    })
+  }
+}
