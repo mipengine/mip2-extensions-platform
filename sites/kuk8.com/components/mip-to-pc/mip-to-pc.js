be797d9cc7987f73b63f8de19fae95f11a74fb39
123liujunyu <39754685+123liujunyu@users.noreply.github.com>
2019-09-09 14:16:23 +0800

    去掉多余注释
    去掉多余注释

--- sites/kuk8.com/components/mip-to-pc/mip-to-pc.js
+++ sites/kuk8.com/components/mip-to-pc/mip-to-pc.js
@@ -1,14 +1,9 @@
 import './index.less'
-//引用mip工具
-let {util} = MIP
-let { platform } = util
-//检查是否是移动设备
+let {util}=MIP
+let {platform }=util
 function shebei() {
-  //识别是否是iOS 系统
   let ios=platform.isIOS()
-  //识别是否是android 系统
   let android=platform.isAndroid()
-  //判断是设备是否移动设备
   if(ios||android){
     return true
   }
@@ -16,20 +11,15 @@ function shebei() {
     return false
   }
 }
-//网址进行替换成PCurl
 function topc (mipurl) {
   let wwwurl=mipurl.replace("mip","www")
   window.location.href=wwwurl
 }
 export default class MIPToPc extends MIP.CustomElement {
   build () {
-    //获取当前页面
     let element = this.element
-    //获取url
     let mipurl = this.element.getAttribute('url')
-    //检测设备
     let isOk = shebei()
-    //如果不是移动设备跳转到PCurl
     if (!isOk) {
       let wwwurl=topc(mipurl)
       element.innerHTML =wwwurl
