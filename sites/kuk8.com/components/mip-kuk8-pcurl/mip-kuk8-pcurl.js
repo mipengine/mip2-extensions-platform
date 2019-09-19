64a6369de2be758491d24f2693604c3e11a28d96
mip-platform <mip-platform@baidu.com>
2019-09-19 13:13:54 +0800

    mip-platform auto create Pull Request

---
+++ sites/kuk8.com/components/mip-kuk8-pcurl/mip-kuk8-pcurl.js
@@ -0,0 +1,27 @@
+import './index.less'
+let { util } = MIP
+let { platform } = util
+function shebei () {
+  let ios = platform.isIOS()
+  let android = platform.isAndroid()
+  if (ios || android) {
+    return true
+  } else {
+    return false
+  }
+}
+function topc (mipurl) {
+  let wwwurl = mipurl.replace('mip', 'www')
+  window.location.href = wwwurl
+}
+export default class MIPKuk8Pcurl extends MIP.CustomElement {
+  build () {
+    let element = this.element
+    let mipurl = this.element.getAttribute('url')
+    let isOk = shebei()
+    if (!isOk) {
+      let wwwurl = topc(mipurl)
+      element.innerHTML = wwwurl
+    }
+  }
+}
