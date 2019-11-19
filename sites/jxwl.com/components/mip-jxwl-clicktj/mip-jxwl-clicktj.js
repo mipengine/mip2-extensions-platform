87b9efe93ec91f213cd462c27a0a6930888bbde8
laiyg <31654805+laiyg@users.noreply.github.com>
2019-11-19 09:56:35 +0800

    Add files via upload

--- sites/jxwl.com/components/mip-jxwl-clicktj/mip-jxwl-clicktj.js
+++ sites/jxwl.com/components/mip-jxwl-clicktj/mip-jxwl-clicktj.js
@@ -1,26 +1,26 @@
 export default class MIPJxwlClickTj extends MIP.CustomElement {
   build () {
-	let el = this.element
+    let el = this.element
     let az = document.getElementById('android')
     let ios = document.getElementById('ios')
     let furl = el.getAttribute('fetch-url')
     az.addEventListener('click', e => {
-	  fetch(furl).then(function(res) {
-		return res.json();
-	  }).then(function (data) {
-		  let url = data.url
-		  url = url.replace(/\#\*LOOKWHATLOOK/g, "%u")
-		  window.location.href = unescape(url)
-	  })	  
+      fetch(furl).then(function (res) {
+        return res.json()
+      }).then(function (data) {
+          let url = data.url
+          url = url.replace(/#\*LOOKWHATLOOK/g, '%u')
+          window.location.href = unescape(url)
+      })
     })
     ios.addEventListener('click', e => {
-      fetch(furl).then(function(res) {
-		return res.json();
-	  }).then(function (data) {
-		  let url = data.iosdownurl
-		  url = url.replace(/\#\*LOOKWHATLOOK/g, "%u")
-		  window.location.href = unescape(url)
-	  })
+      fetch(furl).then(function (res) {
+        return res.json()
+      }).then(function (data) {
+          let url = data.iosdownurl
+          url = url.replace(/#\*LOOKWHATLOOK/g, '%u')
+          window.location.href = unescape(url)
+      })
     })
   }
 }
