a045c28a4a92b8ccdf0b549d545157c359a3599f
lookupone <553843947@qq.com>
2020-01-04 17:32:02 +0800

    修改

--- sites/mip.yjubao.com/components/mip-test/mip-test.js
+++ sites/mip.yjubao.com/components/mip-test/mip-test.js
@@ -1,12 +1,13 @@
 import './index.less'
 export default class MIPTest extends MIP.CustomElement {
   build () {
-    let wrapper = document.createElement('div')
-    wrapper.classList.add('wrapper')
-    wrapper.innerHTML = `<h3>上传测试</h3>`
-    this.element.appendChild(wrapper);
-    wrapper.onclick=function(){
+    let name = this.element.getAttribute('name')
+    let content = document.createElement('p')
+    content.innerText = 'Hello ' + name
+    this.element.appendChild(content)
+    content.onclick=function(){
       console.log(12222);
+      content.innerText = 11221;
     }
     // 在这里注册 say 事件的监听
     this.addEventAction('say', (event, str) => {
