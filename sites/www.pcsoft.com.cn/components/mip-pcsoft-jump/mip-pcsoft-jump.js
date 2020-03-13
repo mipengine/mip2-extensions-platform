ecf7161ff3578ec1b91b4c319d6d122ce827fd11
wanyinga <wy20201118@sina.com>
2020-03-13 18:19:01 +0800

    不合法地方修改

--- sites/www.pcsoft.com.cn/components/mip-pcsoft-jump/mip-pcsoft-jump.js
+++ sites/www.pcsoft.com.cn/components/mip-pcsoft-jump/mip-pcsoft-jump.js
@@ -1,13 +1,12 @@
 /**
  * @file pcsoft网站js判断非移动端访问M站，跳转到www站
  * @author wangying (wangying@njxzwh.com)
- * @update 2020-3-13 js中过多if-else优化
  */
 import './index.less'
 
 export default class MIPExample extends MIP.CustomElement {
   connectedCallback () {
-    let urls = window.location.href.toString()
+    let urls = window.location.href
     // 获取域名
     let domain = urls.split('/')
     if (domain[2]) {
@@ -20,90 +19,87 @@ export default class MIPExample extends MIP.CustomElement {
       if (domain === 'm.pcsoft.com.cn') { // pcsoft
         mprefix = 'https://m.pcsoft.com.cn'
         wprefix = 'https://www.pcsoft.com.cn'
-        let android = urls.split('android/game/')[1]
         let anGame = null
         let sort = null
+        let zt = urls.toString().split('zhuanti/')[1]
+        let article = urls.toString().split('article/')[1]
+        let anSoft = urls.toString().split('android/soft/')[1]
+        let iosGame = urls.toString().split('ios/game/')[1]
+        let iosSoft = urls.toString().split('ios/soft/')[1]
+        let soft = urls.toString().split('soft/')[1]
+        let android = urls.toString().split('android/game/')[1]
         if (android !== undefined && android.indexOf('html') !== -1) { // 安卓游戏详情页
           anGame = android
         } else { // 聚合页（列表）
           sort = android
         }
         let list = '/android/game/' + sort
+        let softDetail = '/soft/' + soft
+        let zuanti = '/zhuanti/' + zt
+        let articleDetail = '/article/' + article
         let angDetail = '/android/game/' + anGame
-        let softDetail = '/soft/' + urls.split('soft/')[1]
-        let zuanti = '/zhuanti/' + urls.split('zhuanti/')[1]
-        let articleDetail = '/article/' + urls.split('article/')[1]
-        let ansDetail = '/android/soft/' + urls.split('android/soft/')[1]
-        let iosgDetail = '/ios/game/' + urls.split('ios/game/')[1]
-        let iossDetail = '/ios/soft/' + urls.split('ios/soft/')[1]
-        let jumpUrl = [{ // 首页
-          startUrl: 'https://m.pcsoft.com.cn/',
-          endUrl: 'https://www.pcsoft.com.cn/'
-        },
-        { // 最近更新
-          startUrl: 'https://m.pcsoft.com.cn/soft/newlist.html',
-          endUrl: 'https://www.pcsoft.com.cn/soft/newlist.html'
-        },
-        { // 专题频道页
-          startUrl: 'https://m.pcsoft.com.cn/zhuanti/',
-          endUrl: 'https://www.pcsoft.com.cn/zhuanti/'
-        },
-        { // 软件频道页
-          startUrl: 'https://m.pcsoft.com.cn/android/soft',
-          endUrl: 'https://www.pcsoft.com.cn/android/soft'
-        },
-        { // 安卓频道页
-          startUrl: 'https://m.pcsoft.com.cn/android/',
-          endUrl: 'https://www.pcsoft.com.cn/android/'
-        },
-        { // 苹果频道页
-          startUrl: 'https://m.pcsoft.com.cn/ios/',
-          endUrl: 'https://www.pcsoft.com.cn/ios/'
-        },
-        { // 资讯频道页
-          startUrl: 'https://m.pcsoft.com.cn/wap_article/',
-          endUrl: 'https://www.pcsoft.com.cn/wap_article/'
-        },
-        { // 聚合页（列表）
-          startUrl: mprefix + list,
-          endUrl: wprefix + list
-        },
-        { // 专题详情页
-          startUrl: mprefix + zuanti,
-          endUrl: wprefix + zuanti
-        },
-        { // 软件详情页
-          startUrl: mprefix + softDetail,
-          endUrl: wprefix + softDetail
-        },
-        { // 安卓软件详情页
-          startUrl: mprefix + ansDetail,
-          endUrl: wprefix + ansDetail
-        },
-        { // 安卓游戏详情页
-          startUrl: mprefix + angDetail,
-          endUrl: wprefix + angDetail
-        },
-        { // 苹果游戏详情页
-          startUrl: mprefix + iosgDetail,
-          endUrl: wprefix + iosgDetail
-        },
-        { // 苹果软件详情页
-          startUrl: mprefix + iossDetail,
-          endUrl: wprefix + iossDetail
-        },
-        { // 资讯详情页
-          startUrl: mprefix + articleDetail,
-          endUrl: wprefix + articleDetail
-        }]
-        jumpUrl.forEach((item) => {
-          if (urls === item.startUrl) {
-            getData(item.startUr, item.endUrl)
-            window.location.href = item.endUrl
-          }
-        })
+        let ansDetail = '/android/soft/' + anSoft
+        let iosgDetail = '/ios/game/' + iosGame
+        let iossDetail = '/ios/soft/' + iosSoft
+        if (urls === mprefix + list && sort !== '') { // 聚合页（列表）
+          getData(mprefix + list, wprefix + list)
+          window.location.href = wprefix + list
+        } else if (urls === mprefix + softDetail && soft !== '') { // 软件详情页
+          getData(mprefix + softDetail, wprefix + softDetail)
+          window.location.href = wprefix + softDetail
+        } else if (urls === mprefix + zuanti && zt !== '') { // 专题详情页
+          getData(mprefix + zuanti, wprefix + zuanti)
+          window.location.href = wprefix + zuanti
+        } else if (urls === mprefix + articleDetail && article !== '') { // 资讯详情页
+          getData(mprefix + articleDetail, wprefix + articleDetail)
+          window.location.href = wprefix + articleDetail
+        } else if (urls === mprefix + iossDetail && iosSoft !== '') { // 苹果软件详情页
+          getData(mprefix + iossDetail, wprefix + iossDetail)
+          window.location.href = wprefix + iossDetail
+        } else if (urls === mprefix + iosgDetail && iosGame !== '') { // 苹果游戏详情页
+          getData(mprefix + iosgDetail, wprefix + iosgDetail)
+          window.location.href = wprefix + iosgDetail
+        } else if (urls === mprefix + ansDetail && anSoft !== '') { // 安卓软件详情页
+          getData(mprefix + ansDetail, wprefix + ansDetail)
+          window.location.href = wprefix + ansDetail
+        } else if (urls === mprefix + angDetail && anGame !== '') { // 安卓游戏详情页
+          getData(mprefix + angDetail, wprefix + angDetail)
+          window.location.href = wprefix + angDetail
+        }
+        switch (urls) {
+          case 'https://m.pcsoft.com.cn/': // 首页
+            getData('https://m.pcsoft.com.cn/', 'https://www.pcsoft.com.cn/')
+            window.location.href = 'https://www.pcsoft.com.cn/'
+            break
+          case 'https://m.pcsoft.com.cn/soft/newlist.html': // 最近更新
+            getData('https://m.pcsoft.com.cn/soft/newlist.html', 'https://www.pcsoft.com.cn/soft/newlist.html')
+            window.location.href = 'https://www.pcsoft.com.cn/soft/newlist.html'
+            break
+          case 'https://m.pcsoft.com.cn/zhuanti/': // 专题频道页
+            getData('https://m.pcsoft.com.cn/zhuanti/', 'https://www.pcsoft.com.cn/zhuanti/')
+            window.location.href = 'https://www.pcsoft.com.cn/zhuanti/'
+            break
+          case 'https://m.pcsoft.com.cn/android/soft/': // 软件频道页
+            getData('https://m.pcsoft.com.cn/android/soft/', 'https://www.pcsoft.com.cn/android/soft/')
+            window.location.href = 'https://www.pcsoft.com.cn/android/soft/'
+            break
+          case 'https://m.pcsoft.com.cn/android/': // 安卓频道页
+            getData('https://m.pcsoft.com.cn/android/', 'https://www.pcsoft.com.cn/android/')
+            window.location.href = 'https://www.pcsoft.com.cn/android/'
+            break
+          case 'https://m.pcsoft.com.cn/ios/': // 苹果频道页
+            getData('https://m.pcsoft.com.cn/ios/', 'https://www.pcsoft.com.cn/ios/')
+            window.location.href = 'https://www.pcsoft.com.cn/ios/'
+            break
+          case 'https://m.pcsoft.com.cn/wap_article/': // 资讯频道页
+            getData('https://m.pcsoft.com.cn/wap_article/', 'https://www.pcsoft.com.cn/wap_article/')
+            window.location.href = 'https://www.pcsoft.com.cn/wap_article/'
+            break
+          default:
+        }
       }
     }
+
     function getData (startUrl, endUrl) { // 跳转统计接口
       fetch('https://mip.js.xzstatic.com/collect', {
         method: 'POST',
