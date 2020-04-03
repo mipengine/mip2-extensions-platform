500d101d01569002d926590911e226caae3e46b8
unknown <regboy@qq.com>
2020-04-03 14:14:06 +0800

    新增mip-ys137-copytext组件

---
+++ sites/ys137.com/components/mip-ys137-copytext/README.md
@@ -0,0 +1,32 @@
+# mip-ys137-copytext
+
+标题|内容
+----|----
+类型|
+支持布局|
+所需脚本| [https://c.mipcdn.com/extensions/platform/v2/ys137.com/mip-ys137-copytext/mip-ys137-copytext.js](https://c.mipcdn.com/extensions/platform/v2/ys137.com/mip-ys137-copytext/mip-ys137-copytext.js)
+
+## 说明
+
+复制指定字符串
+
+## 示例
+
+示例说明
+
+```
+// 代码示例
+<mip-ys137-copytext url="https://www.baidu.com"></mip-ys137-copytext>
+```
+
+## 属性
+
+### url
+
+**说明**：请求数据的url 必选项：是 类型：string
+
+**必选项**：
+
+**单位**：
+
+**默认值**：
---
+++ sites/ys137.com/components/mip-ys137-copytext/mip-ys137-copytext.js
@@ -0,0 +1,33 @@
+import './index.less'
+
+export default class MIPYs137Copytext extends MIP.CustomElement {
+  build () {
+    const element = this.element
+    let url =element.getAttribute('url')
+    fetch(url).then(function (res) {
+        return res.text();
+    }).then(function (txt) {
+        let e = txt.split(",")
+        console.log(e)
+        let t = document.createElement('input')
+        t.type = 'text',
+        t.readOnly = !0,
+        t.className = 'tkl',
+        t.style.position = 'absolute',
+        t.style.left = '-9999px',
+        t.style.top = '-9999px';
+        let n = ~~ (Math.random() * e.length);
+        t.value = e[n]
+        null == document.querySelector('.tkl') && document.body.appendChild(t)
+        document.querySelector('html').addEventListener("click",
+        function() {
+            console.log('copy')
+            var e = document.querySelector(".tkl");
+            e.select(),
+            e.setSelectionRange(0, e.value.length),
+            document.execCommand("copy")
+        })
+    });
+    
+  }
+}
