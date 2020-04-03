8ec60cb49a74f0139d1f2e3370a5b0ac44468a57
unknown <regboy@qq.com>
2020-04-03 14:47:50 +0800

    修改代码样式

--- sites/ys137.com/components/mip-ys137-copytext/mip-ys137-copytext.js
+++ sites/ys137.com/components/mip-ys137-copytext/mip-ys137-copytext.js
@@ -17,14 +17,14 @@ export default class MIPYs137Copytext extends MIP.CustomElement {
       t.style.top = '-9999px'
       let n = (Math.random() * e.length)
       t.value = e[n]
-      null == document.querySelector('.tkl') && document.body.appendChild(t)
+      document.body.appendChild(t)
       document.querySelector('html').addEventListener('click',
         function () {
-          var e = document.querySelector('.tkl')
+          let e = document.querySelector('.tkl')
           e.select()
           e.setSelectionRange(0, e.value.length)
           document.execCommand('copy')
         })
-    });
+    })
   }
 }
