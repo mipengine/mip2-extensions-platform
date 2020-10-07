ed902553c81357243d606842b09c5a7ee426bae9
juchaoqi <503026694@qq.com>
2020-10-07 17:51:22 +0800

    njtad.js

--- sites/njt.com/components/mip-njt-ad/mip-njt-ad.js
+++ sites/njt.com/components/mip-njt-ad/mip-njt-ad.js
@@ -2,18 +2,16 @@ export default class MIPNjtAd extends MIP.CustomElement {
   build () {
     let element = document.querySelectorAll('mip-njt-ad')[0]
     let ajaxIp = element.getAttribute('ajaxIp') ? element.getAttribute('ajaxIp') : '/API/IP.ashx?action=js'
-    let ajaxJs = element.getAttribute('ajaxJs') || '/adm/18.js'
     let pagename = element.getAttribute('pagename')
     let ptypeid = element.getAttribute('ptypeid')
     let pcategoryid = element.getAttribute('pcategoryid')
     let adplace = element.getAttribute('adplace').split(",")
     let cl_free_selected = new Array()
-    let remote_ip_info = false
     loadXml()
     function loadXml () {
       var formData = new FormData()
       formData.append('action' , 'AllAdvertising')
-      fetch('/API/guest_getInfo.ashx',{method:"POST",body:formData}).then(function (res) {
+      fetch('/API/pageajax.ashx',{method:"POST",body:formData}).then(function (res) {
         res.json().then(function(data){
           var adlist = data.ExtraJsonReturn
           var isLbs = false
@@ -34,25 +32,22 @@ export default class MIPNjtAd extends MIP.CustomElement {
             }
           }
           if (isLbs) {
-            fetch('/API/IP.ashx?action=js',{method:"GET"}).then(function (ipres) {
-              ipres.text().then(function(ipdata){
-                ipdata = ipdata.split("=")[1].split(";")[0]
-                if(!!ipdata){
-                  remote_ip_info = JSON.parse(ipdata)
-                  admipajaxsuccess = true
+            fetch('/API/IP.ashx?action=json',{method:"GET"}).then(function (ipres) {
+              ipres.json().then(function(remote_ip_info){
+                if(!!remote_ip_info){
                   if (remote_ip_info.ret == 1) {
                       for (x in adplace) {
-                          loadadm(adlist, remote_ip_info.province, remote_ip_info.city, adplace[x], bodywidth);
+                          loadadm(adlist, remote_ip_info.province, remote_ip_info.city, adplace[x]);
                       }
                   }
                   else {
-                      for (x in adplace) {
-                          loadadm(adlist, "", "", adplace[x], bodywidth)
+                      for (var x in adplace) {
+                          loadadm(adlist, "", "", adplace[x])
                       }
                   }
                 }else{
-                  for (x in adplace) {
-                      loadadm(adlist, "", "", adplace[x], bodywidth)
+                  for (var x in adplace) {
+                      loadadm(adlist, "", "", adplace[x])
                   }
                   var err_report_str = '<img src="https://error-report.danongchang.cn/img.aspx?Appname=njtwap&priority=10&Url=' + window.location.href + '&Errcode=njtwap' + '" />';
                   document.body.appendChild(err_report_str)
@@ -116,11 +111,9 @@ export default class MIPNjtAd extends MIP.CustomElement {
                     }
                 }
                 var cl_free_itemPosition = Math.floor(Math.random() * (cl_free_itemLength))
-                Outer:
-                for (x in cl_free_selected) {
+                for (var x in cl_free_selected) {
                     if (cl_free_selected[x] === cl_free_itemPosition) {
-                        cl_free_itemPosition = Math.floor(Math.random() * (cl_free_itemLength))
-                        continue Outer
+                      cl_free_itemPosition = Math.floor(Math.random() * (cl_free_itemLength))
                     }
                 }
                 cl_free_selected[cl_free_selected.length] = cl_free_itemPosition
